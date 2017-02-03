a8f2b6f1fb0d663dfc0bb60fee04ffe919e30b83
Jose Ramon Macias <jrmacias@ebi.ac.uk>
2017-02-03 15:56:39 +0000

    Add installation and stat info files

---
+++ start_server.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# check paths after installation and change accordingly
+if [ $1 == 'dev' ]
+  then
+    HOME=/home/tc_cm01
+ else
+    HOME=/net/isilonP/public/rw/homes/tc_cm01    
+fi
+APPDIR=$HOME/metabolights/software/MtblsWS-Py
+VENVDIR=$APPDIR/venv
+
+cd $APPDIR
+source $VENVDIR/bin/activate
+python $APPDIR/mtblsWS-Py.py
