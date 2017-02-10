9cff66dce0738784386edcec4f05acea1fb64785
Jose Ramon Macias <jrmacias@ebi.ac.uk>
2017-02-10 12:21:21 +0000

    Remove un-needed HOME settings from start_server.sh script.

--- start_server.sh
+++ start_server.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
 # check paths after installation and change accordingly
-if [[ $1 == "dev" ]]
-  then
-    HOME=/home/tc_cm01
- else
-    HOME=/net/isilonP/public/rw/homes/tc_cm01    
-fi
 APPDIR=$HOME/metabolights/software/MtblsWS-Py
 VENVDIR=$APPDIR/venv
 
