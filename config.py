4bb4d72505dd6f15f564533ca66b59a9f89fc861
Jose Ramon Macias <jrmacias@ebi.ac.uk>
2017-02-16 11:07:53 +0000

    Fix broken dependencies after last file reallocations

--- config.py
+++ config.py
@@ -13,18 +13,20 @@ APP_VERSION = "v1"
 RESOURCES_PATH = "/mtbls/ws/" + APP_VERSION
 API_DOC = RESOURCES_PATH + "/api/spec"
 TEST_DATA_PATH = "testdata"
-STUDIES_INPUT_PATH = "/Users/jrmacias/Projects/Deploy-local/vnas-metabolights/metabolights/test/studies/"
-STUDIES_OUTPUT_PATH = "/Users/jrmacias/Projects/Deploy-local/vnas-metabolights/metabolights/test/userspace/"
 
 MTBLS_WS_HOST = "http://ves-ebi-8d"
 MTBLS_WS_PORT = ":8080"
 MTBLS_WS_RESOURCES_PATH = "/metabolights/webservice"
 
 if ENV == "dev":
+    STUDIES_INPUT_PATH = "/Users/jrmacias/Projects/Deploy-local/vnas-metabolights/metabolights/test/studies/"
+    STUDIES_OUTPUT_PATH = "/Users/jrmacias/Projects/Deploy-local/vnas-metabolights/metabolights/test/userspace/"
     PORT = 5000
     APP_BASE_LINK = "http://localhost:" + str(PORT)
     DEBUG = True
 else:
+    STUDIES_INPUT_PATH = "/net/vnas-metabolights/metabolights/test/studies/"
+    STUDIES_OUTPUT_PATH = "/net/vnas-metabolights/metabolights/test/userspace/"
     PORT = 5000
     APP_BASE_LINK = "https://www.ebi.ac.uk/metabolights"
     DEBUG = False
