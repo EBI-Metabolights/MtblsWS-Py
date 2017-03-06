f35cdc6337516fb2e9fdb9161ff4ff07c65872e0
Jose Ramon Macias <jrmacias@ebi.ac.uk>
2017-03-06 10:34:47 +0000

    Change some values in general config file

--- config.py
+++ config.py
@@ -1,12 +1,10 @@
 import os
 
-# ENV = os.environ.get("ENVIRONMENT", "dev")
-ENV = os.environ.get("ENVIRONMENT", "prod")
 
+PORT = 5000
+APP_BASE_LINK = "https://www.ebi.ac.uk/metabolights"
+DEBUG = False
 PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
-
-STATIC_FOLDER = os.path.join(PROJECT_PATH, "static")
-TEMPLATE_FOLDER = os.path.join(PROJECT_PATH, "templates")
 
 APP_NAME = "MtblsWS-Py"
 APP_DESCRIPTION = "MetaboLights Python-based REST WebService"
@@ -15,16 +13,13 @@ RESOURCES_PATH = "/mtbls/ws"
 API_DOC = RESOURCES_PATH + "/api/spec"
 TEST_DATA_PATH = "testdata"
 
+STATIC_FOLDER = os.path.join(PROJECT_PATH, "static")
+TEMPLATE_FOLDER = os.path.join(PROJECT_PATH, "templates")
+
+# Calling the Java-based MTBLS WS
 MTBLS_WS_HOST = "http://ves-ebi-8d"
 MTBLS_WS_PORT = ":8080"
 MTBLS_WS_RESOURCES_PATH = "/metabolights/webservice"
+
 UPDATE_PATH_SUFFIX = "/audit"
 
-if ENV == "dev":
-    PORT = 5000
-    APP_BASE_LINK = "http://localhost:" + str(PORT)
-    DEBUG = True
-else:
-    PORT = 5000
-    APP_BASE_LINK = "https://www.ebi.ac.uk/metabolights"
-    DEBUG = False
