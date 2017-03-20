ef07912720cb51927cd0a87c8593b15a48ae625d
Jose Ramon Macias <jrmacias@ebi.ac.uk>
2017-03-20 12:14:48 +0000

    Change filed name to make testing easier

--- app/ws/about.py
+++ app/ws/about.py
@@ -15,16 +15,16 @@ date: 2017-03-06
 logger = logging.getLogger('wslog')
 
 app_fields = {
-    'Name': fields.String,
-    'Version': fields.String,
-    'Description': fields.String,
-    'URL': fields.String,
+    'WS-Name': fields.String,
+    'WS-Version': fields.String,
+    'WS-Description': fields.String,
+    'WS-URL': fields.String,
 }
 
 api_fields = {
-    'Version': fields.String,
-    'Documentation': fields.String,
-    'Specification': fields.String,
+    'API-Version': fields.String,
+    'API-Documentation': fields.String,
+    'API-Specification': fields.String,
 }
 
 about_fields = {
@@ -50,14 +50,14 @@ class About(Resource):
     def get(self):
         """Get a basic description of the Web Service"""
         logger.info('Getting WS-about information')
-        api = {"Version": config.API_VERSION,
-               "Documentation": config.WS_APP_BASE_LINK + config.API_DOC + ".html",
-               "Specification": config.WS_APP_BASE_LINK + config.API_DOC + ".json"
+        api = {"API-Version": config.API_VERSION,
+               "API-Documentation": config.WS_APP_BASE_LINK + config.API_DOC + ".html",
+               "API-Specification": config.WS_APP_BASE_LINK + config.API_DOC + ".json"
                }
-        app = {"Name": config.WS_APP_NAME,
-               "Version": config.WS_APP_VERSION,
-               "Description": config.WS_APP_DESCRIPTION,
-               "URL": config.WS_APP_BASE_LINK + config.RESOURCES_PATH,
+        app = {"WS-Name": config.WS_APP_NAME,
+               "WS-Version": config.WS_APP_VERSION,
+               "WS-Description": config.WS_APP_DESCRIPTION,
+               "WS-URL": config.WS_APP_BASE_LINK + config.RESOURCES_PATH,
                }
         about = {'WS-App': app, 'API': api}
         return about
