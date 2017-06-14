05f987123d3ba27d7c253dc1256408dafd63abcb
Jose Ramon Macias <jrmacias@ebi.ac.uk>
2017-06-14 12:28:18 +0100

    Add new PUT method for serialisation of Study Protocols

--- app/ws/isaStudy.py
+++ app/ws/isaStudy.py
@@ -532,50 +532,8 @@ class StudyNew(Resource):
         return inv_obj
 
 
-# TODO ...
-def unserialize_protocols(json_prot_list):
-    protocol_list = list()
-    for json_protocol in json_prot_list:
-
-        if json_protocol['protocol_type'] is not None:
-            protocol_type = OntologyAnnotation(term=json_protocol['protocol_type']['term'])
-        else:
-            protocol_type = OntologyAnnotation()
-
-        parameters = list()
-        if len(json_protocol['parameters']) > 0:
-            for json_parameter in json_protocol['parameters']:
-                parameters.append(ProtocolParameter(parameter_name=json_parameter['parameter_name']))
-
-        components = list()
-        if len(json_protocol['components']) > 0:
-            for comp in json_protocol['components']:
-                components.append(ProtocolComponent(name=comp['name']))
-
-        comments = list()
-        if len(json_protocol['comments']) > 0:
-            for comment in json_protocol['comments']:
-                comments.append(comment)
-        else:
-            comments = []
-
-        new_protocol = Protocol(
-            name=json_protocol['name'],
-            protocol_type=protocol_type,
-            description=json_protocol['description'],
-            uri=json_protocol['uri'],
-            version=json_protocol['version'],
-            parameters=parameters,
-            components=components,
-            comments=comments
-        )
-        protocol_list.append(new_protocol)
-    return protocol_list
-
-
-def serialize_protocol(obj):
-    assert isinstance(obj, Protocol)
-
+def serialize_protocol(isa_obj):
+    assert isinstance(isa_obj, Protocol)
     # name (str):
     # protocol_type (OntologyAnnotation):
     # description (str):
@@ -585,64 +543,207 @@ def serialize_protocol(obj):
     # components (list, OntologyAnnotation):
     # comments (list, str):
     return {
-        'name': obj.name,
-        'protocol_type': json.loads(json.dumps(obj.protocol_type, default=serialize_OntologyAnn, sort_keys=True)),
-        'description': obj.description,
-        'uri': obj.uri,
-        'version': obj.version,
-        'parameters': json.loads(json.dumps(obj.parameters, default=serialize_ProtocolParameter, sort_keys=True)),
-        'components': json.loads(json.dumps(obj.components, default=serialize_OntologyAnn, sort_keys=True)),
-        'comments': obj.comments
+        'name': isa_obj.name,
+        'protocol_type': json.loads(json.dumps(isa_obj.protocol_type, default=serialize_OntologyAnnotation, sort_keys=True)),
+        'description': isa_obj.description,
+        'uri': isa_obj.uri,
+        'version': isa_obj.version,
+        'parameters': json.loads(json.dumps(isa_obj.parameters, default=serialize_ProtocolParameter, sort_keys=True)),
+        'components': json.loads(json.dumps(isa_obj.components, default=serialize_OntologyAnnotation, sort_keys=True)),
+        'comments': isa_obj.comments
     }
 
 
-def serialize_ProtocolParameter(obj):
-    assert isinstance(obj, ProtocolParameter)
+def serialize_OntologyAnnotation(isa_obj):
+    assert isinstance(isa_obj, OntologyAnnotation)
+    # term (str, NoneType):
+    # term_source (OntologySource, NoneType):
+    # term_accession (str, NoneType):
+    # comments (list, NoneType):
+    term_source = None
+    if hasattr(isa_obj, 'term_source') and isa_obj.term_source is not None:
+        term_source = serialize_OntologySource(isa_obj.term_source)
 
+    return {
+        'term': isa_obj.term,
+        'term_source': term_source,
+        'term_accession': isa_obj.term_accession,
+        'comments': isa_obj.comments
+    }
+
+
+def serialize_ProtocolParameter(isa_obj):
+    assert isinstance(isa_obj, ProtocolParameter)
     # name (OntologyAnnotation): A parameter name as a term
     # unit (OntologyAnnotation): A unit, if applicable
     # comments (list, NoneType):
+    parameter_name = None
+    if hasattr(isa_obj, 'parameter_name') and isa_obj.parameter_name is not None:
+        parameter_name = serialize_OntologyAnnotation(isa_obj.parameter_name)
 
-    unit = ''
-    if hasattr(obj, 'unit') and obj.unit is not None:
-        unit = serialize_OntologyAnn(obj.unit),
+    unit = None
+    if hasattr(isa_obj, 'unit') and isa_obj.unit is not None:
+        unit = serialize_OntologyAnnotation(isa_obj.unit)
+
     return {
-        'name': serialize_OntologyAnn(obj.parameter_name),
+        'parameter_name': parameter_name,
         'unit': unit,
-        'comments': obj.comments
+        'comments': isa_obj.comments
     }
 
 
-def serialize_OntologySource(obj):
-    assert isinstance(obj, OntologySource)
-
+def serialize_OntologySource(isa_obj):
+    assert isinstance(isa_obj, OntologySource)
     # name (str):
     # file (str):
     # version (str):
     # description (str):
     # comments (list,):
     return {
-        'name': obj.name,
-        'file': obj.file,
-        'version': obj.version,
-        'description': obj.description,
-        'comments': obj.comments
+        'name': isa_obj.name,
+        'file': isa_obj.file,
+        'version': isa_obj.version,
+        'description': isa_obj.description,
+        'comments': isa_obj.comments
     }
 
 
-def serialize_OntologyAnn(obj):
-    assert isinstance(obj, OntologyAnnotation)
+def unserialize_Protocol(json_protocol):
+    # name (str):
+    # protocol_type (OntologyAnnotation):
+    # description (str):
+    # uri (str):
+    # version (str):
+    # parameters (list, ProtocolParameter):
+    # components (list, OntologyAnnotation):
+    # comments (list, str):
+    name = ''
+    if 'name' in json_protocol and json_protocol['name'] is not None:
+        name = json_protocol['name']
 
+    protocol_type = OntologyAnnotation()
+    if 'protocol_type' in json_protocol and json_protocol['protocol_type'] is not None:
+        protocol_type = unserialize_OntologyAnnotation(json_protocol['protocol_type'])
+
+    description = ''
+    if 'description' in json_protocol and json_protocol['description'] is not None:
+        description = json_protocol['description']
+
+    uri = ''
+    if 'uri' in json_protocol and json_protocol['uri'] is not None:
+        uri = json_protocol['uri']
+
+    version = ''
+    if 'version' in json_protocol and json_protocol['version'] is not None:
+        version = json_protocol['version']
+
+    parameters = list()
+    if 'parameters' in json_protocol:
+        for parameter in json_protocol['parameters']:
+            parameters.append(unserialize_ProtocolParameter(parameter))
+
+    components = list()
+    if len(json_protocol['components']) > 0:
+        for comp in json_protocol['components']:
+            components.append(ProtocolComponent(name=comp['name']))
+
+    comments = list()
+    if 'comments' in json_protocol:
+        for comment in json_protocol['comments']:
+            comments.append(comment)
+
+    return Protocol(name=name,
+                    protocol_type=protocol_type,
+                    description=description,
+                    uri=uri,
+                    version=version,
+                    parameters=parameters,
+                    components=components,
+                    comments=comments)
+
+
+def unserialize_OntologyAnnotation(json_obj):
     # term (str, NoneType):
     # term_source (OntologySource, NoneType):
     # term_accession (str, NoneType):
     # comments (list, NoneType):
-    return {
-        'term': obj.term,
-        'term_source': obj.term_source,
-        'term_accession': obj.term_accession,
-        'comments': obj.comments
-    }
+    term = ''
+    if 'term' in json_obj and json_obj['term'] is not None:
+        term = json_obj['term']
+
+    term_source = None
+    if 'term_source' in json_obj and json_obj['term_source'] is not None:
+        term_source = unserialize_OntologySource(json_obj['term'])
+
+    term_accession = ''
+    if 'term_accession' in json_obj and json_obj['term_accession'] is not None:
+        term_accession = json_obj['term_accession']
+
+    comments = list()
+    if 'comments' in json_obj:
+        for comment in json_obj['comments']:
+            comments.append(comment)
+
+    return OntologyAnnotation(term=term,
+                              term_source=term_source,
+                              term_accession=term_accession,
+                              comments=comments)
+
+
+def unserialize_OntologySource(json_obj):
+    # name (str):
+    # file (str):
+    # version (str):
+    # description (str):
+    # comments (list,):
+    name = ''
+    if 'name' in json_obj and json_obj['name'] is not None:
+        name = json_obj['name']
+
+    file = ''
+    if 'name' in json_obj and json_obj['file'] is not None:
+        file = json_obj['file']
+
+    version = ''
+    if 'version' in json_obj and json_obj['version'] is not None:
+        version = json_obj['version']
+
+    description = ''
+    if 'description' in json_obj and json_obj['description'] is not None:
+        description = json_obj['description']
+
+    comments = list()
+    if 'comments' in json_obj:
+        for comment in json_obj['comments']:
+            comments.append(comment)
+
+    return OntologySource(name=name,
+                          file=file,
+                          version=version,
+                          description=description,
+                          comments='')
+
+
+def unserialize_ProtocolParameter(json_obj):
+    # name (OntologyAnnotation): A parameter name as a term
+    # unit (OntologyAnnotation): A unit, if applicable
+    # comments (list, NoneType):
+    parameter_name = OntologyAnnotation()
+    if 'parameter_name' in json_obj and json_obj['parameter_name'] is not None:
+        parameter_name = unserialize_OntologyAnnotation(json_obj['parameter_name'])
+
+    unit = OntologyAnnotation()
+    if 'unit' in json_obj and json_obj['unit'] is not None:
+        unit = unserialize_OntologyAnnotation(json_obj['unit'])
+
+    comments = list()
+    if 'comments' in json_obj:
+        for comment in json_obj['comments']:
+            comments.append(comment)
+
+    return ProtocolParameter(parameter_name=parameter_name,
+                             unit=unit,
+                             comments=comments)
 
 
 class StudyProtocols(Resource):
@@ -705,8 +806,8 @@ class StudyProtocols(Resource):
         wsc.is_study_public(study_id, user_token)
 
         logger.info('Getting Study protocols for %s, using API-Key %s', study_id, user_token)
-        protocols = iac.get_study_protocols(study_id, user_token)
-        str_protocols = json.dumps({'Study-protocols': protocols}, default=serialize_protocol, sort_keys=True)
+        json_protocols = iac.get_study_protocols(study_id, user_token)
+        str_protocols = json.dumps({'Study-protocols': json_protocols}, default=serialize_protocol, sort_keys=True)
         logger.info('Got: %s', str_protocols)
 
         return json.loads(str_protocols)
@@ -716,7 +817,7 @@ class StudyProtocols(Resource):
         summary='Update MTBLS Study protocols',
         notes="""Update the protocols of the MTBLS Study with {study_id}.
                       Only the new protocols (in JSON format) must be provided in the body of the request.
-                      i.e.: { "protocols": "Updated Study protocols..." }""",
+                      i.e.: { "Study-protocols": [ {...}, {...}]}""",
         parameters=[
             {
                 "name": "study_id",
@@ -736,8 +837,12 @@ class StudyProtocols(Resource):
             },
             {
                 "name": "protocols",
-                "description": """New protocols in JSON format.</br>
-             i.e.: { "protocols": "New Study protocols..." }""",
+                "description": """
+                New protocols in JSON format.</br>
+                i.e.: { "Study-protocols": [ 
+                </br>{...}, 
+                </br>{...}</br>]}
+                """,
                 "paramType": "body",
                 "type": "string",
                 "format": "application/json",
@@ -794,9 +899,12 @@ class StudyProtocols(Resource):
         if request.data is None or request.json is None:
             abort(400)
         data_dict = json.loads(request.data.decode('utf-8'))
-        new_protocols = data_dict['Study-protocols']
+        json_protocols = data_dict['Study-protocols']
 
-        prots_list = unserialize_protocols(new_protocols)
+        isa_protocols = list()
+        for json_protocol in json_protocols:
+            isa_protocol = unserialize_Protocol(json_protocol)
+            isa_protocols.append(isa_protocol)
 
         # check for keeping copies
         save_audit_copy = True
@@ -810,10 +918,7 @@ class StudyProtocols(Resource):
         else:
             logging.warning("A copy of the previous file will NOT be saved")
 
-        # TODO ---
-        # iac.write_study_json_protocols(study_id, user_token, prots_list, save_audit_copy)
-        logger.info('Applied %s', new_protocols)
+        iac.write_study_json_protocols(study_id, user_token, isa_protocols, save_audit_copy)
+        logger.info('Applied %s', json_protocols)
 
-        # return jsonify({"Study-protocols": new_protocols})
-        return jsonify({"Study-protocols": prots_list})
-
+        return jsonify({"Study-protocols": json_protocols})
