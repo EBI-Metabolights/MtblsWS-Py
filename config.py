84926fc831469560990a90ce7fcaad8b31bda533
Jose Ramon Macias <jrmacias@ebi.ac.uk>
2017-07-24 16:30:01 +0100

    Open CORS for testint purposes

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ WS_APP_DESCRIPTION = "MetaboLights Python-based REST WebService"
 #   MAJOR version when backwards incompatible changes are introduced
 #   MINOR version when new functionality is added in a backwards-compatible manner
 #   PATCH version when bugs are fixed (but still backwards-compatible)
-WS_APP_VERSION = "0.7.2"
+WS_APP_VERSION = "0.7.3"
 RESOURCES_PATH = "/mtbls/ws"
 
 TEST_DATA_PATH = "testdata"
