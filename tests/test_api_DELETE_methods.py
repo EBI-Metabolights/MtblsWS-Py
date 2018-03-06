3cdc0af96bcfdb458a6c54c45586a426916c26af
Jose Ramon Macias <jrmacias@ebi.ac.uk>
2018-03-06 12:31:35 +0000

    Add unit tests for CRUD methods for Study Publications

--- tests/test_api_DELETE_methods.py
+++ tests/test_api_DELETE_methods.py
@@ -37,6 +37,18 @@ class WsTests(unittest.TestCase):
     def check_body_common(self, body):
         self.assertIsNotNone(body)
 
+    def check_OntologySource_class(self, obj):
+        self.assertIsNotNone(obj['name'])
+        self.assertIsNotNone(obj['description'])
+        self.assertIsNotNone(obj['file'])
+        self.assertIsNotNone(obj['version'])
+
+    def check_OntologyAnnotation_class(self, obj):
+        self.assertIsNotNone(obj['annotationValue'])
+        if obj['termSource']:
+            self.check_OntologySource_class(obj['termSource'])
+        self.assertIsNotNone(obj['termAccession'])
+        self.assertIsNotNone(obj['comments'])
 
 # ############################################
 # All tests that DELETE data below this point
@@ -65,9 +77,7 @@ class DeleteStudyContactTests(WsTests):
         self.assertIsNotNone(obj['fax'])
         self.assertIsNotNone(obj['comments'])
         for role in obj['roles']:
-            self.assertIsNotNone(role['annotationValue'])
-            self.assertIsNotNone(role['termAccession'])
-            self.assertIsNotNone(role['comments'])
+            self.check_OntologyAnnotation_class(role)
 
     def pre_create_contact(self, url):
         request = urllib.request.Request(url + '/contacts',
@@ -306,14 +316,23 @@ class DeleteStudyProtocolTests(WsTests):
     def tearDown(self):
         time.sleep(1)  # sleep time in seconds
 
+    def check_ProtocolParameter_class(self, obj):
+        if obj['parameterName']:
+            self.check_OntologyAnnotation_class(obj['parameterName'])
+        self.assertIsNotNone(obj['comments'])
+
     def check_Protocol_class(self, obj):
         self.assertIsNotNone(obj['name'])
-        self.assertIsNotNone(obj['protocolType'])
+        if obj['protocolType']:
+            self.check_OntologyAnnotation_class(obj['protocolType'])
         self.assertIsNotNone(obj['description'])
         self.assertIsNotNone(obj['uri'])
         self.assertIsNotNone(obj['version'])
-        self.assertIsNotNone(obj['parameters'])
-        self.assertIsNotNone(obj['components'])
+        for param in obj['parameters']:
+            self.check_ProtocolParameter_class(param)
+        if obj['components']:
+            self.check_OntologyAnnotation_class(obj['components'])
+        self.assertIsNotNone(obj['comments'])
 
     def pre_create_protocol(self, url):
         request = urllib.request.Request(url + '/protocols',
@@ -552,9 +571,10 @@ class DeleteStudyFactorTests(WsTests):
     def tearDown(self):
         time.sleep(1)  # sleep time in seconds
 
-    def check_Factor_class(self, obj):
+    def check_StudyFactor_class(self, obj):
         self.assertIsNotNone(obj['factorName'])
-        self.assertIsNotNone(obj['factorType'])
+        if obj['factorType']:
+            self.check_OntologyAnnotation_class(obj['factorType'])
         self.assertIsNotNone(obj['comments'])
 
     def pre_create_factor(self, url):
@@ -601,7 +621,7 @@ class DeleteStudyFactorTests(WsTests):
             self.assertIn('factor', body)
             j_resp = json.loads(body)
             self.assertIsNotNone(j_resp['factor'])
-            self.check_Factor_class(j_resp['factor'])
+            self.check_StudyFactor_class(j_resp['factor'])
 
     # Delete Study Factor - Pub - NoToken -> 401
     def test_delete_Factor_pub_auth_noToken(self):
@@ -702,7 +722,7 @@ class DeleteStudyFactorTests(WsTests):
             self.assertIn('factor', body)
             j_resp = json.loads(body)
             self.assertIsNotNone(j_resp['factor'])
-            self.check_Factor_class(j_resp['factor'])
+            self.check_StudyFactor_class(j_resp['factor'])
 
     # Delete Study Factor - Priv - NoToken -> 401
     def test_delete_Factor_priv_auth_noToken(self):
@@ -783,7 +803,7 @@ class DeleteStudyFactorTests(WsTests):
             self.assertEqual('NOT FOUND', err.reason)
 
 
-class DeleteStudyyDesignDescriptorTests(WsTests):
+class DeleteStudyDesignDescriptorTests(WsTests):
 
     valid_id = instance.config.VALID_ID_DESCRIPTOR
     bad_id = instance.config.BAD_ID_DESCRIPTOR
@@ -793,12 +813,6 @@ class DeleteStudyyDesignDescriptorTests(WsTests):
 
     def tearDown(self):
         time.sleep(1)  # sleep time in seconds
-
-    def check_DesignDescriptor_class(self, obj):
-        self.assertIsNotNone(obj['annotationValue'])
-        self.assertIsNotNone(obj['termSource'])
-        self.assertIsNotNone(obj['termAccession'])
-        self.assertIsNotNone(obj['comments'])
 
     def pre_create_descriptor(self, url):
         request = urllib.request.Request(url + '/descriptors',
@@ -844,7 +858,7 @@ class DeleteStudyyDesignDescriptorTests(WsTests):
             self.assertIn('studyDesignDescriptor', body)
             j_resp = json.loads(body)
             self.assertIsNotNone(j_resp['studyDesignDescriptor'])
-            self.check_DesignDescriptor_class(j_resp['studyDesignDescriptor'])
+            self.check_OntologyAnnotation_class(j_resp['studyDesignDescriptor'])
 
     # Delete Study Descriptor - Pub - NoToken -> 401
     def test_delete_descriptor_pub_auth_noToken(self):
@@ -945,7 +959,7 @@ class DeleteStudyyDesignDescriptorTests(WsTests):
             self.assertIn('studyDesignDescriptor', body)
             j_resp = json.loads(body)
             self.assertIsNotNone(j_resp['studyDesignDescriptor'])
-            self.check_DesignDescriptor_class(j_resp['studyDesignDescriptor'])
+            self.check_OntologyAnnotation_class(j_resp['studyDesignDescriptor'])
 
     # Delete Study Descriptor - Priv - NoToken -> 401
     def test_delete_descriptor_priv_auth_noToken(self):
@@ -1013,6 +1027,252 @@ class DeleteStudyyDesignDescriptorTests(WsTests):
     def test_delete_descriptor_priv_auth_unknownFactor(self):
         request = urllib.request.Request(url_priv_id + '/descriptors'
                                          + '?annotationValue=' + self.bad_id,
+                                         method='DELETE')
+        self.add_common_headers(request)
+        request.add_header('user_token', auth_id)
+        try:
+            urllib.request.urlopen(request)
+        except urllib.error.HTTPError as err:
+            self.assertEqual(err.code, 404)
+            self.check_header_common(err.headers)
+            self.check_body_common(err.read().decode('utf-8'))
+            self.assertEqual('NOT FOUND', err.msg)
+            self.assertEqual('NOT FOUND', err.reason)
+
+
+class DeleteStudyPublicationTests(WsTests):
+
+    valid_id = instance.config.VALID_ID_PUBLICATION
+    bad_id = instance.config.BAD_ID_PUBLICATION
+    valid_data = instance.config.TEST_DATA_VALID_PUBLICATION
+    missing_data = instance.config.TEST_DATA_MISSING_PUBLICATION
+    no_data = b''
+
+    def tearDown(self):
+        time.sleep(1)  # sleep time in seconds
+
+    def check_Publication_class(self, obj):
+        self.assertIsNotNone(obj['title'])
+        self.assertIsNotNone(obj['authorList'])
+        self.assertIsNotNone(obj['pubMedID'])
+        self.assertIsNotNone(obj['doi'])
+        if obj['status']:
+            self.check_OntologyAnnotation_class(obj['status'])
+        self.assertIsNotNone(obj['comments'])
+
+    def pre_create_publication(self, url):
+        request = urllib.request.Request(url + '/publications',
+                                         data=self.valid_data, method='POST')
+        self.add_common_headers(request)
+        request.add_header('user_token', auth_id)
+        try:
+            urllib.request.urlopen(request)
+        except urllib.error.HTTPError as err:
+            if err.code != 409:
+                raise Exception(err)
+
+    def pre_delete_publication(self, url):
+        request = urllib.request.Request(url + '/publications'
+                                         + '?title=' + self.valid_id,
+                                         method='DELETE')
+        self.add_common_headers(request)
+        request.add_header('user_token', auth_id)
+        try:
+            urllib.request.urlopen(request)
+        except urllib.error.HTTPError as err:
+            if err.code != 404:
+                raise Exception(err)
+
+    # Delete Publication - Pub - Auth -> 200
+    def test_delete_publication_pub_auth(self):
+        # first, create the publication to ensure it will exists
+        self.pre_create_publication(url_pub_id)
+        time.sleep(1)  # sleep time in seconds
+
+        # then, try to delete the publication
+        request = urllib.request.Request(url_pub_id + '/publications'
+                                         + '?title=' + self.valid_id,
+                                         method='DELETE')
+        self.add_common_headers(request)
+        request.add_header('user_token', auth_id)
+        with urllib.request.urlopen(request) as response:
+            self.assertEqual(response.code, 200)
+            header = response.info()
+            self.check_header_common(header)
+            body = response.read().decode('utf-8')
+            self.check_body_common(body)
+            self.assertIn('publication', body)
+            j_resp = json.loads(body)
+            self.assertIsNotNone(j_resp['publication'])
+            self.check_Publication_class(j_resp['publication'])
+
+    # Delete Publication - Pub - NoToken -> 401
+    def test_delete_publication_pub_auth_noToken(self):
+        request = urllib.request.Request(url_pub_id + '/publications'
+                                         + '?title=' + self.valid_id,
+                                         method='DELETE')
+        self.add_common_headers(request)
+        try:
+            urllib.request.urlopen(request)
+        except urllib.error.HTTPError as err:
+            self.assertEqual(err.code, 401)
+            self.check_header_common(err.headers)
+            self.check_body_common(err.read().decode('utf-8'))
+            self.assertEqual('UNAUTHORIZED', err.msg)
+            self.assertEqual('UNAUTHORIZED', err.reason)
+
+    # Delete Publication - Pub - NoAuth -> 403
+    def test_delete_publication_pub_noAuth(self):
+        request = urllib.request.Request(url_pub_id + '/publications'
+                                         + '?title=' + self.valid_id,
+                                         method='DELETE')
+        self.add_common_headers(request)
+        request.add_header('user_token', wrong_auth_token)
+        try:
+            urllib.request.urlopen(request)
+        except urllib.error.HTTPError as err:
+            self.assertEqual(err.code, 403)
+            self.check_header_common(err.headers)
+            self.check_body_common(err.read().decode('utf-8'))
+            self.assertEqual('FORBIDDEN', err.msg)
+            self.assertEqual('FORBIDDEN', err.reason)
+
+    # Delete Publication - Pub - Auth - NoParams -> 404
+    def test_delete_publication_pub_auth_noParams(self):
+        request = urllib.request.Request(url_pub_id + '/publications',
+                                         method='DELETE')
+        self.add_common_headers(request)
+        request.add_header('user_token', auth_id)
+        try:
+            urllib.request.urlopen(request)
+        except urllib.error.HTTPError as err:
+            self.assertEqual(err.code, 404)
+            self.check_header_common(err.headers)
+            self.check_body_common(err.read().decode('utf-8'))
+            self.assertEqual('NOT FOUND', err.msg)
+            self.assertEqual('NOT FOUND', err.reason)
+
+    # Delete Publication - Pub - Auth - NoData -> 404
+    def test_delete_publication_pub_auth_noData(self):
+        request = urllib.request.Request(url_pub_id + '/publications'
+                                         + '?title=',
+                                         method='DELETE')
+        self.add_common_headers(request)
+        request.add_header('user_token', auth_id)
+        try:
+            urllib.request.urlopen(request)
+        except urllib.error.HTTPError as err:
+            self.assertEqual(err.code, 404)
+            self.check_header_common(err.headers)
+            self.check_body_common(err.read().decode('utf-8'))
+            self.assertEqual('NOT FOUND', err.msg)
+            self.assertEqual('NOT FOUND', err.reason)
+
+    # Delete Publication - Pub - Auth - unknownDescriptor -> 404
+    def test_delete_publication_pub_auth_unknownDescriptor(self):
+        request = urllib.request.Request(url_pub_id + '/publications'
+                                         + '?title=' + self.bad_id,
+                                         method='DELETE')
+        self.add_common_headers(request)
+        request.add_header('user_token', auth_id)
+        try:
+            urllib.request.urlopen(request)
+        except urllib.error.HTTPError as err:
+            self.assertEqual(err.code, 404)
+            self.check_header_common(err.headers)
+            self.check_body_common(err.read().decode('utf-8'))
+            self.assertEqual('NOT FOUND', err.msg)
+            self.assertEqual('NOT FOUND', err.reason)
+
+    # Delete Publication - Priv - Auth - NewData -> 200
+    def test_delete_publication_priv_auth_newData(self):
+        # first, create the publication to ensure it will exists
+        self.pre_create_publication(url_priv_id)
+        time.sleep(1)  # sleep time in seconds
+
+        # then, try to delete the publication
+        request = urllib.request.Request(url_priv_id + '/publications'
+                                         + '?title=' + self.valid_id,
+                                         method='DELETE')
+        self.add_common_headers(request)
+        request.add_header('user_token', auth_id)
+        with urllib.request.urlopen(request) as response:
+            self.assertEqual(response.code, 200)
+            header = response.info()
+            self.check_header_common(header)
+            body = response.read().decode('utf-8')
+            self.check_body_common(body)
+            self.assertIn('publication', body)
+            j_resp = json.loads(body)
+            self.assertIsNotNone(j_resp['publication'])
+            self.check_Publication_class(j_resp['publication'])
+
+    # Delete Publication - Priv - NoToken -> 401
+    def test_delete_publication_priv_auth_noToken(self):
+        request = urllib.request.Request(url_priv_id + '/publications'
+                                         + '?title=' + self.valid_id,
+                                         method='DELETE')
+        self.add_common_headers(request)
+        try:
+            urllib.request.urlopen(request)
+        except urllib.error.HTTPError as err:
+            self.assertEqual(err.code, 401)
+            self.check_header_common(err.headers)
+            self.check_body_common(err.read().decode('utf-8'))
+            self.assertEqual('UNAUTHORIZED', err.msg)
+            self.assertEqual('UNAUTHORIZED', err.reason)
+
+    # Delete Publication - Priv - NoAuth -> 403
+    def test_delete_publication_priv_noAuth(self):
+        request = urllib.request.Request(url_priv_id + '/publications'
+                                         + '?title=' + self.valid_id,
+                                         method='DELETE')
+        self.add_common_headers(request)
+        request.add_header('user_token', wrong_auth_token)
+        try:
+            urllib.request.urlopen(request)
+        except urllib.error.HTTPError as err:
+            self.assertEqual(err.code, 403)
+            self.check_header_common(err.headers)
+            self.check_body_common(err.read().decode('utf-8'))
+            self.assertEqual('FORBIDDEN', err.msg)
+            self.assertEqual('FORBIDDEN', err.reason)
+
+    # Delete Publication - Priv - Auth - NoParams -> 404
+    def test_delete_publication_priv_auth_noParams(self):
+        request = urllib.request.Request(url_priv_id + '/publications',
+                                         method='DELETE')
+        self.add_common_headers(request)
+        request.add_header('user_token', auth_id)
+        try:
+            urllib.request.urlopen(request)
+        except urllib.error.HTTPError as err:
+            self.assertEqual(err.code, 404)
+            self.check_header_common(err.headers)
+            self.check_body_common(err.read().decode('utf-8'))
+            self.assertEqual('NOT FOUND', err.msg)
+            self.assertEqual('NOT FOUND', err.reason)
+
+    # Delete Publication - Priv - Auth - NoData -> 404
+    def test_delete_publication_piv_auth_noData(self):
+        request = urllib.request.Request(url_priv_id + '/publications'
+                                         + '?title=',
+                                         method='DELETE')
+        self.add_common_headers(request)
+        request.add_header('user_token', auth_id)
+        try:
+            urllib.request.urlopen(request)
+        except urllib.error.HTTPError as err:
+            self.assertEqual(err.code, 404)
+            self.check_header_common(err.headers)
+            self.check_body_common(err.read().decode('utf-8'))
+            self.assertEqual('NOT FOUND', err.msg)
+            self.assertEqual('NOT FOUND', err.reason)
+
+    # Delete Publication - Priv - Auth - unknownDescriptor -> 404
+    def test_delete_publication_priv_auth_unknownFactor(self):
+        request = urllib.request.Request(url_priv_id + '/publications'
+                                         + '?title=' + self.bad_id,
                                          method='DELETE')
         self.add_common_headers(request)
         request.add_header('user_token', auth_id)
