c447cb1b5300dad228104ea70cb5412c87a09e64
JKChang2015 <jkchang2015@gmail.com>
2018-07-12 13:05:00 +0100

    change the import package

--- app/ws/ontology.py
+++ app/ws/ontology.py
@@ -10,7 +10,7 @@ from flask import current_app as app
 from flask import request, jsonify
 from flask_restful import Resource, abort, reqparse
 from flask_restful_swagger import swagger
-from owlready2 import *
+from owlready2 import get_ontology
 
 from app.ws.isaApiClient import IsaApiClient
 from app.ws.mtblsWSclient import WsClient
