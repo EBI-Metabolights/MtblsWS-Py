f9b7e1f828d65a4c4633c08c097c27ebf129a7ac
Kenneth Haug <khaug@yahoo.com>
2018-10-23 09:45:10 +0100

    - Added obfuscation_code to the study files method

--- app/ws/mtblsStudy.py
+++ app/ws/mtblsStudy.py
@@ -375,6 +375,7 @@ class StudyFiles(Resource):
 
         logger.info('Getting list of all files for MTBLS Study %s', study_id)
         study_location = wsc.get_study_location(study_id, user_token)
+        obfuscation_code = wsc.get_study_obfuscation(study_id, user_token)
         data_dict = json.loads(wsc.create_upload_folder(study_id, user_token))
         upload_location = data_dict["message"]
         logger.info('Getting list of all files for MTBLS Study %s. Study folder: %s. Upload folder: %s', study_id,
@@ -382,7 +383,8 @@ class StudyFiles(Resource):
         study_files = get_all_files(study_location)
         upload_files = get_all_files(upload_location)
         upload_location = upload_location.split('/mtblight')  # FTP/Aspera root starts here
-        return jsonify({'studyFiles': study_files, 'upload': upload_files, 'upload_location': upload_location[1]})
+
+        return jsonify({'studyFiles': study_files, 'upload': upload_files, 'upload_location': upload_location[1], 'obfuscation_code': obfuscation_code})
 
 
 class AllocateAccession(Resource):
