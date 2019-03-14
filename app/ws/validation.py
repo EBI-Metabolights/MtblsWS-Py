0d797e68d403ef5f587d41f2278b3cda4c907885
Kenneth Haug <khaug@yahoo.com>
2019-03-14 17:24:24 +0000

    - Added more validation rules

--- app/ws/validation.py
+++ app/ws/validation.py
@@ -15,16 +15,40 @@ error = "error"
 success = "success"
 
 
-def add_msg(validations, section, message, status):
-    validations.append({section: message, "status": status})
+def add_msg(validations, section, message, status, value="", desrc=""):
+    validations.append({section: message, "status": status, "value": value, "desciption": desrc})
 
 
-def get_validation_rules(validation_schema, sub_part):
+def get_basic_validation_rules(validation_schema, part):
     if validation_schema:
         study_val = validation_schema['study']
-        val = study_val[sub_part]
+        val = study_val[part]
         rules = val['rules'][0]
-    return rules
+    return rules, val['description']
+
+
+def get_complex_validation_rules(validation_schema, part, sub_part, sub_set):
+    if validation_schema:
+        study_val = validation_schema['study']
+        val = study_val[part]
+        sub = val[sub_part]
+        sets = sub[sub_set]
+        rules = sets['rules'][0]
+    return rules, sets['description']
+
+
+def extract_details(rule):
+    try:
+        try:
+            val = int(rule['value'])
+        except:
+            val = rule['value']
+        val_error = rule['error']
+        val_condition = rule['condition']
+        val_type = rule['type']
+    except:
+        return int(0), "n/a", "n/a", "n/a"
+    return val, val_error, val_condition, val_type
 
 
 class Validation(Resource):
@@ -115,16 +139,6 @@ def validate_study(study_id, study_location, user_token):
     status, amber_warning, isa_meta_validation = validate_isa_tab_metadata(isa_inv, isa_study, validation_schema)
     all_validations.append(isa_meta_validation)
 
-    # status, validation.append(validate_protocols())
-    # status, validation.append(validate_protocols())
-    # status, validation.append(validate_protocols())
-    # status, validation.append(validate_protocols())
-    # status, validation.append(validate_protocols())
-    # status, validation.append(validate_protocols())
-    # status, validation.append(validate_protocols())
-    # status, validation.append(validate_protocols())
-    # status, validation.append(validate_protocols())
-
     if status:
         if amber_warning:
             return {"validations": all_validations, "study_validation_status": warning}
@@ -140,90 +154,121 @@ def validate_publication(isa_study, validation_schema):
     amber_warning = False
     validations = []
 
+    title_rules, title_val_description = get_complex_validation_rules(
+        validation_schema, part='publications', sub_part='publication', sub_set='title')
+    title_val_len, title_val_error, title_val_condition, title_val_type = extract_details(title_rules)
+
     if isa_study.publications:
         amber_warning = False
         pmid = False
         doi = False
 
+        pmid_rules, pmid_val_description = get_complex_validation_rules(
+            validation_schema, part='publications', sub_part='publication', sub_set='pubMedID')
+        pmid_val_len, pmid_val_error, pmid_val_condition, pmid_val_type = extract_details(pmid_rules)
+
+        doi_rules, doi_val_description = get_complex_validation_rules(
+            validation_schema, part='publications', sub_part='publication', sub_set='doi')
+        doi_val, doi_val_error, doi_val_condition, doi_val_type = extract_details(doi_rules)
+
+        author_rules, author_val_description = get_complex_validation_rules(
+            validation_schema, part='publications', sub_part='publication', sub_set='authorList')
+        author_val_len, author_val_error, author_val_condition, author_val_type = extract_details(author_rules)
+
+        status_rules, status_val_description = get_complex_validation_rules(
+            validation_schema, part='publications', sub_part='publication', sub_set='status')
+        status_val_len, status_val_error, status_val_condition, status_val_type = extract_details(status_rules)
+
         for index, publication in enumerate(isa_study.publications):
-            #ToDo, check doi and pmid strings
 
             if not publication:
-                validations.append({"Publication": "Publication is missing. Please provide a tentative title of your paper",
-                                    "status": error})
+                add_msg(validations, "Publication", title_val_error, error)
                 validates = False
             else:
-                validations.append({"Publication": "Found your paper",
-                                    "status": success})
+                add_msg(validations, "Publication", "Found a publication", success)
 
             if not publication.title:
-                validations.append({"Publication title": "Please provide a tentative title of your paper",
-                                    "status": error})
+                add_msg(validations, "Publication title", title_val_error, error)
                 validates = False
             elif publication.title:
-                validations.append({"Publication title": "Found the title of your paper",
-                                    "status": success})
+                if len(publication.title) >= title_val_len:
+                    add_msg(validations, "Publication title", "Found the title of the publication", success)
+                else:
+                    add_msg(validations, "Publication title", title_val_error, warning,
+                            value=publication.title, desrc=title_val_description)
 
             if not publication.doi:
-                validations.append({"Publication DOI": "Please provide a doi for your paper",
-                                    "status": warning})
+                add_msg(validations, "Publication DOI", doi_val_description, warning)
                 doi = False
                 amber_warning = True
             elif publication.doi:
-                validations.append({"Publication DOI": "Found the doi for your paper",
-                                    "status": success})
-                doi = True
+                doi = publication.doi
+                doi_pattern = re.compile(doi_val)
+                doi_check = doi_pattern.match(doi)
+                if doi_check:
+                    add_msg(validations, "Publication DOI", "Found the doi for the publication", success)
+                    doi = True
+                else:
+                    add_msg(validations, "Publication DOI", doi_val_error, error, doi)
+                    doi = False
 
             if not publication.pubmed_id:
-                validations.append({"Publication PubMed ID": "Please provide a pmid for your paper",
-                                    "status": warning})
+                add_msg(validations, "Publication PubMed ID", pmid_val_description, warning)
                 amber_warning = True
                 pmid = False
             elif publication.pubmed_id:
-                validations.append({"Publication PubMed ID": "Found the pmid for your paper",
-                                    "status": success})
-                pmid = True
+                if int(publication.pubmed_id) >= int(pmid_val_len):
+                    add_msg(validations, "Publication PubMed ID", "Found the pmid for the publication", success)
+                    pmid = True
+                else:
+                    add_msg(validations, "Publication PubMed ID", pmid_val_error, error,
+                            value=publication.pubmed_id, desrc=pmid_val_description)
+                    pmid = False
 
             if not doi or not pmid:
-                validations.append(
-                    {"Publication doi and PubMed ID": "Please provide both a doi and pmid for your paper",
-                     "status": warning})
+                add_msg(validations, "Publication doi and PubMed ID",
+                        "Please provide both a valid doi and pmid for the publication", warning)
                 amber_warning = True
             elif doi and pmid:
-                validations.append(
-                    {"Publication doi and PubMed ID ": "Found both doi and pmid for your paper",
-                     "status": success})
+                add_msg(validations, "Publication doi and PubMed ID ",
+                        "Found both doi and pmid for the publication", success)
 
             if not publication.author_list:
-                validations.append({"Publication author list": "Please provide an author list",
-                                    "status": error})
+                add_msg(validations, "Publication author list", author_val_description, error)
                 validates = False
             elif publication.author_list:
-                validations.append({"Publication author list": "Found the author list",
-                                    "status": success})
+                if len(publication.author_list) >= author_val_len:
+                    add_msg(validations, "Publication author list",
+                            "Found the author list for the publication", success)
+                else:
+                    add_msg(validations, "Publication author list", author_val_error, error,
+                            value=publication.author_list, desrc=author_val_description)
+                    validates = False
 
             if not publication.status:
-                validations.append({"Publication status": "Please provide the publication status",
-                                    "status": error})
+                add_msg(validations, "Publication status", "Please provide the publication status", error)
                 validates = False
             elif publication.status:
-                validations.append({"Publication status": "Found the publication status",
-                                    "status": success})
+                pub_status = publication.status
+                if len(pub_status.term) >= status_val_len:
+                    add_msg(validations, "Publication status", "Found the publication status", success)
+                else:
+                    add_msg(validations, "Publication status", status_val_error, success,
+                            value=pub_status.title, desrc=status_val_description)
     else:
-        validations.append({"Publication missing": "Please provide a publication using the tentative title of your paper",
-                            "status": error})
+        add_msg(validations, "Publication missing", title_val_error, error)
         validates = False
 
     if not validates:
-        ret_list = {"publication": validations, "overall_status": "Validation failed",
-                     "overall_status": error}
+        ret_list = {"publication": validations, "status_message": "Validation failed",
+                    "overall_status": error}
     elif amber_warning:
         ret_list = {"publication": validations,
-                     "overall_status": "Some optional information is missing for your study",
-                     "overall_status": warning}
+                    "status_message": "Some optional information is missing for your study",
+                    "overall_status": warning}
     else:
-        ret_list = {"publication": validations, "overall_status": "Successful validation",
-                     "overall_status": success}
+        ret_list = {"publication": validations, "status_message": "Successful validation",
+                    "overall_status": success}
 
     return validates, amber_warning, ret_list
 
@@ -238,71 +283,59 @@ def validate_basic_isa_tab(study_id, user_token, study_location):
                                                      study_location=study_location)
 
     if isa_inv:
-        validations.append({"ISA-Tab": "Successfully read the i_Investigation.txt files", "status": success})
+        add_msg(validations, "ISA-Tab", "Successfully read the i_Investigation.txt files", success)
 
         if isa_study:
-            validations.append({"ISA-Tab": "Successfully read the study section of the investigation file",
-                                "status": success})
+            add_msg(validations, "ISA-Tab", "Successfully read the study section of the investigation file", success)
         else:
-            validations.append({"ISA-Tab": "Can not read the study section of the investigation file",
-                                "status": error})
+            add_msg(validations, "ISA-Tab", "Can not read the study section of the investigation file", error)
             validates = False
 
         if isa_study.filename:
-            validations.append({"ISA-Tab": "Successfully found the reference to the sample sheet filename",
-                                "status": success})
+            add_msg(validations, "ISA-Tab", "Successfully found the reference to the sample sheet filename", success)
         else:
-            validations.append({"ISA-Tab": "Could not find the reference to the sample sheet filename",
-                                "status": error})
+            add_msg(validations, "ISA-Tab", "Could not find the reference to the sample sheet filename", error)
             validates = False
 
         if validates:  # Have to have a basic investigation and sample file before we can continue
             if isa_study.samples:
-                validations.append({"ISA-Tab": "Successfully found one or more samples",
-                                    "status": success})
+                add_msg(validations, "ISA-Tab", "Successfully found one or more samples", success)
             else:
-                validations.append({"ISA-Tab": "Could not find any samples",
-                                    "status": error})
+                add_msg(validations, "ISA-Tab", "Could not find any samples", error)
                 validates = False
 
             if isa_study.assays:
-                validations.append({"ISA-Tab": "Successfully found one or more assays",
-                                    "status": success})
+                add_msg(validations, "ISA-Tab", "Successfully found one or more assays", success)
             else:
-                validations.append({"ISA-Tab": "Could not find any assays",
-                                    "status": error})
+                add_msg(validations, "ISA-Tab", "Could not find any assays", error)
                 validates = False
 
             if isa_study.factors:
-                validations.append({"ISA-Tab": "Successfully found one or more factors",
-                                    "status": success})
+                add_msg(validations, "ISA-Tab", "Successfully found one or more factors", success)
             else:
-                validations.append({"ISA-Tab": "Could not find any factors",
-                                    "status": warning})
+                add_msg(validations, "ISA-Tab", "Could not find any factors", warning)
                 amber_warning = True
 
             if isa_study.design_descriptors:
-                validations.append({"ISA-Tab": "Successfully found one or more descriptors",
-                                    "status": success})
+                add_msg(validations, "ISA-Tab", "Successfully found one or more descriptors", success)
             else:
-                validations.append({"ISA-Tab": "Could not find any study design descriptors",
-                                    "status": error})
+                add_msg(validations, "ISA-Tab", "Could not find any study design descriptors", error)
                 validates = False
 
     else:
-        validations.append({"ISA-Tab": "Can not find or read the investigation files", "status": error})
+        add_msg(validations, "ISA-Tab", "Can not find or read the investigation files", error)
         validates = False
 
     if not validates:
         ret_list = {"isa-tab": validations, "status_message": "Validation failed",
-                     "overall_status": error}
+                    "overall_status": error}
     elif amber_warning:
         ret_list = {"isa-tab": validations,
-                     "status_message": "Some optional information is missing for your study",
-                     "overall_status": warning}
+                    "status_message": "Some optional information is missing for your study",
+                    "overall_status": warning}
     else:
         ret_list = {"isa-tab": validations, "status_message": "Successful validation",
-                     "overall_status": success}
+                    "overall_status": success}
 
     return isa_study, isa_inv, std_path, validates, amber_warning, ret_list
 
@@ -313,35 +346,35 @@ def validate_isa_tab_metadata(isa_inv, isa_study, validation_schema):
     validations = []
 
     if validation_schema:
-        title_rules = get_validation_rules(validation_schema, 'title')
-        desc_rules = get_validation_rules(validation_schema, 'description')
+        title_rules, title_descr = get_basic_validation_rules(validation_schema, 'title')
+        desc_rules, desc_desrc = get_basic_validation_rules(validation_schema, 'description')
 
     if isa_inv:
 
         # Title
-        title_val_len = int(title_rules['value'])
+        val_len, val_error, val_condition, val_type = extract_details(title_rules)
         try:
-            title_len = len(isa_inv.title)
+            title_len = len(isa_study.title)
         except:
             title_len = 0
 
-        if title_len >= title_val_len:
+        if title_len >= val_len:
             add_msg(validations, "Metadata", "The title length validates", success)
         else:
-            add_msg(validations, "Metadata", title_rules['error'], error)
+            add_msg(validations, "Metadata", val_error, error, value=isa_study.title, desrc=title_descr)
             validates = False
 
         # Description
-        desc_val_len = int(desc_rules['value'])
+        val_len, val_error, val_condition, val_type = extract_details(desc_rules)
         try:
-            descr_len = len(isa_inv.description)
+            descr_len = len(isa_study.description)
         except:
             descr_len = 0
 
-        if descr_len >= desc_val_len:
+        if descr_len >= val_len:
             add_msg(validations, "Metadata", "The length of the description validates", success)
         else:
-            add_msg(validations, "Metadata", desc_rules['error'], error)
+            add_msg(validations, "Metadata", val_error, error, value=isa_study.description, desrc=desc_desrc)
             validates = False
 
     else:
