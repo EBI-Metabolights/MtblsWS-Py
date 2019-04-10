987a68e07a4a9b0f992989e171e5030d967b1b2a
Kenneth Haug <khaug@yahoo.com>
2019-04-10 16:41:34 +0100

    - Creating curator spreadsheet for ChEBI submission

--- app/ws/chebi_workflow.py
+++ app/ws/chebi_workflow.py
@@ -2,11 +2,11 @@ import logging, pandas as pd, os
 import numpy as np
 import requests
 import cirpy
-import string
-from flask import request, abort
+from flask import request, abort, current_app as app
 from flask_restful import Resource, reqparse
 from flask_restful_swagger import swagger
 from pubchempy import get_compounds
+from chemspipy import ChemSpider
 from app.ws.mtblsWSclient import WsClient
 from app.ws.utils import read_tsv, write_tsv
 from app.ws.mtbls_maf import totuples, get_table_header
@@ -86,9 +86,20 @@ def search_and_update_maf(study_location, annotation_file_name):
     row_idx = 0
     # Search using the compound name
     for idx, comp_name in enumerate(maf_df[maf_compound_name_column]):
-        search_res = wsc.get_maf_search("name", comp_name)
-        pc_name, pc_inchi, pc_inchi_key, pc_smiles, pc_cid, pc_formula = pubchem_search(comp_name)
+        print(comp_name)
 
+        pc_name, pc_inchi, pc_inchi_key, pc_smiles, pc_cid, pc_formula, pc_synonyms = pubchem_search(comp_name)
+
+        cactus_stdinchikey = cactus_search(comp_name, 'stdinchikey')
+        opsin_stdinchikey = opsin_search(comp_name, 'stdinchikey')
+        cactus_smiles = cactus_search(comp_name, 'smiles')
+        opsin_smiles = opsin_search(comp_name, 'smiles')
+        cactus_inchi = cactus_search(comp_name, 'stdinchi')
+        opsin_inchi = opsin_search(comp_name, 'stdinchi')
+        cactus_synonyms = cactus_search(comp_name, 'names')  # Synonyms
+        csid = get_csid(pc_inchi_key, cactus_stdinchikey)
+
+        search_res = wsc.get_maf_search("name", comp_name)  # This is the standard MetaboLights aka Plugin search
         if search_res['content']:
             name = None
             result = search_res['content'][0]
@@ -103,27 +114,8 @@ def search_and_update_maf(study_location, annotation_file_name):
             pubchem_df.iloc[row_idx, 2] = smiles
             pubchem_df.iloc[row_idx, 3] = inchi
             # 4 is name / metabolite_identification from MAF
-            pubchem_df.iloc[row_idx, 5] = pc_name
-            pubchem_df.iloc[row_idx, 6] = pc_cid
-            # 7 PubChem CID from InChIKey search (Cactus, OBSIN)
-            # 8 ChemSpider ID (CSID) from INCHIKEY
-            # 9 final smiles
-            # 10 final inchi
-            # 11 final inchikey
-            pubchem_df.iloc[row_idx, 12] = pc_smiles
-            pubchem_df.iloc[row_idx, 13] = catus_search(comp_name, 'smiles')
-            pubchem_df.iloc[row_idx, 14] = opsin_search(comp_name, 'smiles')
 
-            pubchem_df.iloc[row_idx, 15] = pc_inchi
-            pubchem_df.iloc[row_idx, 16] = catus_search(comp_name, 'stdinchi')
-            pubchem_df.iloc[row_idx, 17] = opsin_search(comp_name, 'stdinchi')
-
-            pubchem_df.iloc[row_idx, 18] = pc_inchi_key
-            pubchem_df.iloc[row_idx, 19] = catus_search(comp_name, 'stdinchikey')
-            pubchem_df.iloc[row_idx, 20] = opsin_search(comp_name, 'stdinchikey')
-            pubchem_df.iloc[row_idx, 21] = pc_formula
-
-            if name:  # comp_name == name:  # We have an exact match
+            if name:
                 if database_identifier:
                     maf_df.iloc[row_idx, int(standard_maf_columns['database_identifier'])] = database_identifier
                 if chemical_formula:
@@ -132,12 +124,79 @@ def search_and_update_maf(study_location, annotation_file_name):
                     maf_df.iloc[row_idx, int(standard_maf_columns['smiles'])] = smiles
                 if inchi:
                     maf_df.iloc[row_idx, int(standard_maf_columns['inchi'])] = inchi
-            row_idx += 1
+
+        pubchem_df.iloc[row_idx, 5] = pc_name  # 5 PubChem name
+        pubchem_df.iloc[row_idx, 6] = pc_cid   # 6 PubChem CID
+
+        if not pc_cid:
+            pc_cid = get_pubchem_cid_on_inchikey(cactus_stdinchikey, opsin_stdinchikey)
+        pubchem_df.iloc[row_idx, 7] = pc_cid  # 7 PubChem CID, if none get from InChIKey search (Cactus, OBSIN)
+        pubchem_df.iloc[row_idx, 8] = csid # 8 ChemSpider ID (CSID) from INCHI
+        pubchem_df.iloc[row_idx, 9] = get_ranked_values(pc_smiles, cactus_smiles, opsin_smiles, None)  # 9 final smiles
+        pubchem_df.iloc[row_idx, 10] = get_ranked_values(pc_inchi, cactus_inchi, opsin_inchi, None)  # 10 final inchi
+        pubchem_df.iloc[row_idx, 11] = get_ranked_values(pc_inchi_key, cactus_stdinchikey,
+                                                         opsin_stdinchikey, None)  # 11 final inchikey
+        pubchem_df.iloc[row_idx, 12] = pc_smiles  # 12 pc_smiles
+        pubchem_df.iloc[row_idx, 13] = cactus_smiles   # 13 cactus_smiles
+        pubchem_df.iloc[row_idx, 14] = opsin_smiles  # 14 opsin_smiles
+        pubchem_df.iloc[row_idx, 15] = pc_inchi  # 15 PubChem inchi
+        pubchem_df.iloc[row_idx, 16] = cactus_inchi  # 16 Cacus inchi
+        pubchem_df.iloc[row_idx, 17] = opsin_inchi   # 17 Opsin inchi
+        pubchem_df.iloc[row_idx, 18] = pc_inchi_key  # 18 PubChem stdinchikey
+        pubchem_df.iloc[row_idx, 19] = cactus_stdinchikey  # 19 cactus_stdinchikey
+        pubchem_df.iloc[row_idx, 20] = opsin_stdinchikey   # 20 opsin_stdinchikey
+        pubchem_df.iloc[row_idx, 21] = pc_formula   # 21 PubChem formula
+        pubchem_df.iloc[row_idx, 22] = pc_synonyms  # 22 PubChem synonyms
+        pubchem_df.iloc[row_idx, 23] = cactus_synonyms  # 23 Cactus synonyms
+
+        row_idx += 1
 
     write_tsv(maf_df, short_file_name + "_annotated.tsv")
     write_tsv(pubchem_df, short_file_name + "_pubchem.tsv")
 
     return maf_df, maf_len, new_maf_df, new_maf_len
+
+
+def get_csid(inchikey1, inchikey2):
+    csid = ""
+    csurl_base = 'http://parts.chemspider.com/JSON.ashx?op='
+
+    if [inchikey1, inchikey2]:
+        for inchikey in [inchikey1, inchikey2]:
+            if inchikey:
+                url1 = csurl_base + 'SimpleSearch&searchOptions.QueryText=' + inchikey
+                resp1 = requests.get(url1)
+                if resp1.status_code == 200:
+                    url2 = csurl_base + 'GetSearchResult&rid=' + resp1.text
+                    resp2 = requests.get(url2)
+                    if resp2.status_code == 200:
+                        csid = resp2.text
+                        return csid.replace('[', '').replace(']', '')
+    return csid
+
+
+def get_pubchem_cid_on_inchikey(inchikey1, inchikey2):
+    pc_cid = ''
+    for inchikey in [inchikey1, inchikey2]:
+        if inchikey:
+            pc_name, pc_inchi, pc_inchi_key, pc_smiles, pc_cid, pc_formula, pc_synonyms = \
+                pubchem_search(inchikey, search_type='inchikey')
+            if pc_cid:
+                return pc_cid
+    return pc_cid
+
+
+def get_ranked_values(pubchem, cactus, opsin, chemspider):
+    if pubchem:
+        return pubchem
+    elif cactus:
+        return cactus
+    elif opsin:
+        return opsin
+    elif chemspider:
+        return chemspider
+    else:
+        return ""
 
 
 def create_pubchem_df(maf_df):
@@ -165,6 +224,8 @@ def create_pubchem_df(maf_df):
     pubchem_df['opsin_inchi_key'] = ''      # 20
 
     pubchem_df['pubchem_formula'] = ''      # 21
+    pubchem_df['pubchem_synonyms'] = ''  # 22
+    pubchem_df['cactus_synonyms'] = ''  # 23
 
     return pubchem_df
 
@@ -180,26 +241,84 @@ def opsin_search(comp_name, req_type):
     return result
 
 
-def catus_search(comp_name, type):
+def cactus_search(comp_name, type):
     result = cirpy.resolve(comp_name, type)
-
+    synonyms = ""
     if result:
         if type == 'stdinchikey':
             return result.replace('InChIKey=', '')
+        if type == 'names':
+            for synonym in result:
+                if get_relevant_synonym(synonym):
+                    synonyms = synonyms + ';' + synonym
+            return synonyms
+
     return result
 
 
-def pubchem_search(comp_name):
+def get_relevant_synonym(synonym):
+
+    if synonym.startswith('CAS-'):
+        synonym = synonym.replace('CAS-', '').replace('-', '')
+        return is_correct_int(synonym, 6)
+
+    elif synonym.startswith('HMDB'):
+        return is_correct_int(synonym.replace('HMDB', ''), 7)
+
+    elif synonym.startswith('LM'):  # LipidMaps
+        synonym = synonym[4:]
+        return is_correct_int(synonym, 8)
+
+    elif synonym.startswith('YMDB'):
+        return is_correct_int(synonym.replace('YMDB', ''), 5)
+
+    elif synonym.startswith('ECMDB'):
+        return is_correct_int(synonym.replace('ECMDB', ''), 5)
+
+    elif synonym.startswith('ECMDB'):
+        return is_correct_int(synonym.replace('ECMDB', ''), 5)
+
+    elif synonym.startswith('C'):  # KEGG Compound
+        return is_correct_int(synonym.replace('C', ''), 5)
+
+    elif synonym.startswith('D'):  # KEGG Drug
+        return is_correct_int(synonym.replace('D', ''), 5)
+
+    elif synonym.startswith('G'):  # KEGG Glycan
+        return is_correct_int(synonym.replace('G', ''), 5)
+
+    elif synonym.startswith('R'):  # KEGG Reaction
+        return is_correct_int(synonym.replace('R', ''), 5)
+
+    elif synonym.startswith('HSDB '):  # HSDB/TOXNET
+        return is_correct_int(synonym.replace('HSDB ', ''), 4)
+
+    else:
+        return False
+
+
+def is_correct_int(num, length):
+    try:
+        if len(num) == length:
+            int(num)
+            return True
+    except:
+        return False
+    return False
+
+
+def pubchem_search(comp_name, search_type='name'):
     iupac = ''
     inchi = ''
     inchi_key = ''
     smiles = ''
     cid = ''
     formula = ''
+    synonyms = ''
 
     # For this to work on Mac, run: cd "/Applications/Python 3.6/"; sudo "./Install Certificates.command
     try:
-        pubchem_compound = get_compounds(comp_name, namespace='name')
+        pubchem_compound = get_compounds(comp_name, namespace=search_type)
         compound = pubchem_compound[0]  # Only read the first record from PubChem = preferred entry
         inchi = compound.inchi
         inchi_key = compound.inchikey
@@ -207,11 +326,14 @@ def pubchem_search(comp_name):
         iupac = compound.iupac_name
         cid = compound.cid
         formula = compound.molecular_formula
+        for synonym in compound.synonyms:
+            if get_relevant_synonym(synonym):
+                synonyms = synonyms + ';' + synonym
         logger.debug('Searching PubChem for "' + comp_name + '", got cid "' + cid + '" and iupac name "' + iupac + '"')
     except:
         logger.error("Unable to search PubChem for compound " + comp_name)
 
-    return iupac, inchi, inchi_key, smiles, cid, formula
+    return iupac, inchi, inchi_key, smiles, cid, formula, synonyms
 
 
 class SplitMaf(Resource):
