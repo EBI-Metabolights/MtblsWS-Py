1d08dab22f2d4817b0b2c37e84d82343b8e68a20
Kenneth Haug <khaug@yahoo.com>
2019-04-15 09:53:47 +0100

    - Updated startup script (again!)

--- start_gunicorn.sh
+++ start_gunicorn.sh
@@ -19,5 +19,4 @@ source $VENVDIR/bin/activate
 
 # kill all processes, just in case
 killall gunicorn
-gunicorn --workers 3 --threads 2 -b 0.0.0.0:5005 --worker-class gevent --pid ./app_$(uname -n).pid --preload wsapp:app --access-logfile ./logs/gunicorn_$(uname -n).log --error-logfile ./logs/gunicorn_$(un
-ame -n).log --log-level info --capture-output --daemon > ./logs/logs_$(uname -n).txt 2>&1 & echo $! > app_$(uname -n).pid
+gunicorn --workers 3 --threads 2 -b 0.0.0.0:5005 --worker-class gevent --pid ./app_$(uname -n).pid --preload wsapp:app --access-logfile ./logs/gunicorn_$(uname -n).log --error-logfile ./logs/gunicorn_$(uname -n).log --log-level info --capture-output --daemon > ./logs/logs_$(uname -n).txt 2>&1 & echo $! > app_$(uname -n).pid
