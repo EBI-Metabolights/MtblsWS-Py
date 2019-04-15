011213594a8b9535832bc82d6b26aa098706c352
Kenneth Haug <khaug@yahoo.com>
2019-04-15 09:50:06 +0100

    - Updated startup script (again!)

--- start_gunicorn.sh
+++ start_gunicorn.sh
@@ -1,17 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env bash
+. ../../functions.inc
+HOST=`hostname`
+echo $HOST
+check_host .
+echo "Host $HOST approved, starting Green Unicorn server"
 
 APPDIR=$PWD
-VENVDIR=$APPDIR/venv351
+VENVDIR=$APPDIR/venv363
 
 PYTHONPATH=$APPDIR
 cd $APPDIR
 
-# activate RH collections for Python 3.5.1
-source scl_source enable rh-python35
+# activate RH collections for Python 3.6.3
+source scl_source enable rh-python36
 
 # activate Python virtual environment and launch WS
 source $VENVDIR/bin/activate
 
 # kill all processes, just in case
 killall gunicorn
-gunicorn --workers 3 --threads 2 -b 0.0.0.0:5005 --worker-class gevent --pid ./app_$(uname -n).pid --preload wsapp:app --max-requests 100 --access-logfile ./logs/gunicorn_$(uname -n).log --error-logfile ./logs/gunicorn_$(uname -n).log --log-level info --capture-output --daemon > ./logs/logs_$(uname -n).txt 2>&1 & echo $! > app_$(uname -n).pid
+gunicorn --workers 3 --threads 2 -b 0.0.0.0:5005 --worker-class gevent --pid ./app_$(uname -n).pid --preload wsapp:app --access-logfile ./logs/gunicorn_$(uname -n).log --error-logfile ./logs/gunicorn_$(un
+ame -n).log --log-level info --capture-output --daemon > ./logs/logs_$(uname -n).txt 2>&1 & echo $! > app_$(uname -n).pid
