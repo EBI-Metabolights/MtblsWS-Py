b667fffb407d52667759a7abd4e7f4a4dba6f316
Kenneth Haug <khaug@yahoo.com>
2019-06-03 14:01:29 +0100

    Adding copyright

--- app/ws/spectra.py
+++ app/ws/spectra.py
@@ -1,3 +1,21 @@
+#  EMBL-EBI MetaboLights - https://www.ebi.ac.uk/metabolights
+#  Metabolomics team
+#
+#  European Bioinformatics Institute (EMBL-EBI), European Molecular Biology Laboratory, Wellcome Genome Campus, Hinxton, Cambridge CB10 1SD, United Kingdom
+#
+#  Last modified: 2019-Jun-03
+#  Modified by:   kenneth
+#
+#  Copyright 2019 EMBL - European Bioinformatics Institute
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+
 import pyopenms
 import logging
 import json
@@ -48,7 +66,7 @@ class ExtractMSSpectra(Resource):
             },
             {
                 "name": "mtbls_compound_id",
-                "description": "MetaboLights compound id (MTBLCxxxxxx)",
+                "description": "MetaboLights compound id (MTBLCxxxxx)",
                 "required": True,
                 "allowEmptyValue": False,
                 "allowMultiple": False,
