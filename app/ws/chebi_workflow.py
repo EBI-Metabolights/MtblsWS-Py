86bd4e35a2909d48df5b33f9a3dec40646364dc7
Kenneth Haug <khaug@yahoo.com>
2020-01-07 10:16:29 +0000

    - New log file for ChEBI pipeline - ChEBI pipeline bug. First synonym was not reset for each loop

--- app/ws/chebi_workflow.py
+++ app/ws/chebi_workflow.py
@@ -19,7 +19,6 @@
 import glob
 import logging
 import os
-import pandas as pd
 import re
 import shlex
 import ssl
@@ -32,6 +31,7 @@ from subprocess import *
 import cirpy
 import ctfile
 import numpy as np
+import pandas as pd
 import pronto
 import pubchempy as pcp
 import requests
@@ -1470,6 +1470,7 @@ def pubchem_search(comp_name, search_type='name', search_category='compound'):
             cid = str(cid).strip().rstrip('\n')
             formula = compound.molecular_formula.strip().rstrip('\n')
             for idx, synonym in enumerate(compound.synonyms):
+                first_synonym = None
                 if idx == 0 and synonym.lower() != comp_name.lower():
                     # The first synonym is also the PubChem name,
                     # so always add this to the return set unless it's the same as the compound name we searched for
