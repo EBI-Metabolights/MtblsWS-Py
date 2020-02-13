15ed539f85c6b144972bb33e7cf3d706a0ef8d2f
AjaySharmaEBI <ajaysharma@ebi.ac.uk>
2020-02-13 12:17:22 +0000

    adding setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='MtblsWS-Py',
+    version='1.1',
+    packages=['app', 'app.ws', 'tests'],
+    url='',
+    license='',
+    author='Metabolights ',
+    author_email='ajaysharma@ebi.ac.uk',
+    description='metabolights python based webservices '
+)
