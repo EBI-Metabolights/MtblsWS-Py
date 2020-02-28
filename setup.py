90f78f7b6b0ba39c4a9f67f3fa783aa943e82281
Kenneth Haug <khaug@yahoo.com>
2020-02-28 10:42:04 +0000

    Added sqllite3 settings to Gunicorn startup script

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='app',
-    version='1.4.5',
+    version='1.4.6',
     packages=['app', 'app.ws', 'tests'],
     url='https://github.com/EBI-Metabolights/MtblsWS-Py',
     license='http://www.apache.org/licenses/LICENSE-2.0',
