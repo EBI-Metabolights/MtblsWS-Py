96edd4ccef2d83cf7f14c16328ec9ee6ca8c8034
Kenneth Haug <khaug@yahoo.com>
2020-05-02 10:18:28 +0100

    Bug: "proc" can not be used for empty file patterns to ignore

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ TEMPLATE_FOLDER = os.path.join(PROJECT_PATH, "templates")
 #   MAJOR version when backwards incompatible changes are introduced
 #   MINOR version when new functionality is added in a backwards-compatible manner
 #   PATCH version when bugs are fixed (but still backwards-compatible)
-WS_APP_VERSION = "1.6.26"
+WS_APP_VERSION = "1.6.27"
 API_VERSION = WS_APP_VERSION
 ISA_API_VERSION = "0.11.0"
 METASPACE_API_VERSION = "1.7.2"
@@ -115,8 +115,8 @@ IGNORE_FILE_LIST = ['msprofile', '_func', '_chroms', '_header', 'defaultmasscal'
                     'devices.xml', '_inlet', '_extern', 'synchelper', 'title', 'msts.xml', 'metexplore_mapping',
                     'tempbase', 'prosol_history', 'validation_files', 'pulseprogram', '_history', 'tcc_1',
                     'msactualdefs', 'msmasscal', 'fq1list', 'pdata', 'uxnmr', 'shimvalues', 'specpar', 'output',
-                    'format.temp', 'scon2', '.output', 'stanprogram', 'proc', 'precom', 'settings', 'outd', 'gpnam',
-                    'base_info', 'clevels']
+                    'format.temp', 'scon2', 'stanprogram', 'precom', 'settings', 'outd', 'gpnam', 'base_info',
+                    'clevels']
 
 
 RAW_FILES_LIST = ['.d', '.raw', '.idb', '.cdf', '.wiff', '.scan', '.dat', '.cmp', '.cdf.cmp',
--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools import setup
 
 setup(
     name='app',
-    version='1.6.26',
+    version='1.6.27',
     packages=['app', 'app.ws', 'tests'],
     url='https://github.com/EBI-Metabolights/MtblsWS-Py',
     license='http://www.apache.org/licenses/LICENSE-2.0',
