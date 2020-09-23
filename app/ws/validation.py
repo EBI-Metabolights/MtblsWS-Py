a4b2b5916fb131ea60799a81f32c8d4f401bed7e
Venkata Chandrasekhar <mailcs76@gmail.com>
2020-09-23 18:36:28 +0100

    Fetching the raw files checklist in validations from the config

--- app/ws/validation.py
+++ app/ws/validation.py
@@ -1338,7 +1338,7 @@ def validate_files(study_id, study_location, obfuscation_code, override_list, fi
                         "are present in the study folder: '" + file_name + "'",
                         error, val_section, value=file_name, val_sequence=6.1, log_category=log_category)
 
-        if file_type in ('raw', 'fid', 'acqus'):  # Raw for MS and fid/acqus for NMR
+        if file_type in app.config.get('RAW_FILES_LIST'):  # Raw for MS and fid/acqus for NMR
             raw_file_found = True
 
         if file_type == 'derived':
