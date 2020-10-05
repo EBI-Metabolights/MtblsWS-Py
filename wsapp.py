fa0b1f4b33a7230fab76ababf8f51963debf027d
JKChang2015 <jkchang2015@gmail.com>
2020-10-05 21:41:10 +0100

    - new endpoint: fella pathway - fella.R script - fella.sh script - set up cluster params

--- wsapp.py
+++ wsapp.py
@@ -55,7 +55,7 @@ from app.ws.study_files import StudyFiles, StudyFilesTree, SampleStudyFiles, Unz
 from app.ws.table_editor import *
 from app.ws.user_management import UserManagement
 from app.ws.validation import Validation, OverrideValidation, UpdateValidationFile
-from app.ws.pathway import keggid
+from app.ws.pathway import keggid,fellaPathway
 
 """
 MTBLS WS-Py
@@ -193,6 +193,7 @@ def initialize_app(flask_app):
 
     api.add_resource(cronjob, res_path + "/ebi-internal/cronjob")
     api.add_resource(keggid,res_path+"/ebi-internal/keggid")
+    api.add_resource(fellaPathway,res_path+"/ebi-internal/fella-pathway")
 
     # https://www.ebi.ac.uk:443/metabolights/ws/v2
     api.add_resource(reports, res_path + "/v2/reports")
