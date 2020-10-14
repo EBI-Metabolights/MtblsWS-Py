d722834234417bf7503c87e42458c83d14d40ab2
JKChang2015 <jkchang2015@gmail.com>
2020-10-15 00:41:14 +0100

    - fella.R update

--- resources/fella.R
+++ resources/fella.R
@@ -35,43 +35,51 @@ if (!dir.exists(path.hsa)) {
   gc()
 }
 
-extract_KEGG_id <- function(mtbls_id) {
-  url <- paste('http://wp-np3-15.ebi.ac.uk:5000/metabolights/ws/ebi-internal/keggid?studyID=',mtbls_id,'&kegg_only=true',sep='')
+extract_KEGG_id <- function(mtbls_id,org) {
+  url <-
+    paste0(
+      'http://wp-np3-15.ebi.ac.uk:5000/metabolights/ws/ebi-internal/keggid?studyID=',
+      mtbls_id,
+      '&kegg_only=true')
   dy <- POST(url)
   data = fromJSON(rawToChar(dy$content))
-  kegg.ids <- unique(data)
+  kegg.ids <-unique(data[org][[1]])
   return (kegg.ids)
 }
 
-kegg.ids <- extract_KEGG_id (mtbls_id)
+kegg.ids <- extract_KEGG_id (mtbls_id,org)
 
-res <- FELLA::enrich(
-  compounds = kegg.ids, methods = "diffusion",
-  databaseDir = path.hsa, internalDir = FALSE)
+if(length(kegg.ids) > 0){
+  res <- FELLA::enrich(
+    compounds = kegg.ids, methods = "diffusion",
+    databaseDir = path.hsa, internalDir = FALSE)
 
-g <- FELLA::generateResultsGraph(
-  format = "igraph", object = res$user, data = res$data)
+  g <- FELLA::generateResultsGraph(
+    format = "igraph", object = res$user, data = res$data)
 
-mapSolidColor <- c(`1` = "#CD0000", 
-                   `2` = "#CD96CD", 
-                   `3` = "#FFA200",
-                   `4` = "#8DB6CD",
-                   `5` = "#548B54")
+  mapSolidColor <- c(`1` = "#CD0000",
+                     `2` = "#CD96CD",
+                     `3` = "#FFA200",
+                     `4` = "#8DB6CD",
+                     `5` = "#548B54")
 
-V(g)$shape <- rep("dot", vcount(g))
-V(g)$shape[V(g)$input] <- "square"
-V(g)$color <- mapSolidColor[V(g)$com]
+  V(g)$shape <- rep("dot", vcount(g))
+  V(g)$shape[V(g)$input] <- "square"
+  V(g)$color <- mapSolidColor[V(g)$com]
 
-# Visualize the graph
-vg<- visNetwork::visIgraph(g, idToLabel = FALSE)
+  # Visualize the graph
+  vg<- visNetwork::visIgraph(g, idToLabel = FALSE)
 
-t <- FELLA::generateResultsTable(method = 'diffusion',
-                                 nlimit = 1000,
-                                 object = res$user,
-                                 data=res$data)
+  t <- FELLA::generateResultsTable(method = 'diffusion',
+                                   nlimit = 1000,
+                                   object = res$user,
+                                   data=res$data)
 
-o_g <- paste(output_folder,mtbls_id,'.html',sep='')
-htmlwidgets::saveWidget(vg, o_g)
+  o_g <- paste0(output_folder,mtbls_id,'_',org,'.html')
+  htmlwidgets::saveWidget(vg, o_g)
 
-o_t <-  paste(output_folder,mtbls_id,'.csv',sep='')
-write.csv(t,file=o_t)
+  o_t <-  paste0(output_folder,mtbls_id,'_',org,'.csv')
+  write.csv(t,file=o_t)
+}else{
+  message("No KEGG ID provided")
+}
