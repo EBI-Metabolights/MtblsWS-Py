9bee7d464376b32e54fafee25150ebb73b5fa79c
JKChang2015 <jkchang2015@gmail.com>
2021-04-27 11:15:17 +0100

    - add msp to DERIVED_FILES_LIST

--- config.py
+++ config.py
@@ -142,7 +142,7 @@ RAW_FILES_LIST = ['.d', '.raw', '.idb', '.cdf', '.wiff', '.scan', '.dat', '.cmp'
                   '.lcd', '.abf', '.jpf', '.xps', '.mgf', '.qgd', '.hr']
 
 DERIVED_FILES_LIST = ['.mzml', '.nmrml', '.mzxml', '.xml', '.mzdata', '.cef', '.cnx', '.peakml', '.xy', '.smp',
-                      '.scan', '.dx', '.DX']
+                      '.scan', '.dx', '.DX','.msp']
 
 COMPRESSED_FILES_LIST = ['.zip', 'zipx', '.gz', '.cdf.gz', '.tar', '.7z', '.z', '.g7z', '.arj', '.rar',
                          '.bz2', '.arj', '.z', '.war', '.raw.rar']
