c793d8fd9a3b8ce96f686faa66fbdb46dce61982
cmartin <callumisveryawesome@gmail.com>
2021-12-06 13:33:13 +0000

    fixed typo

--- wsapp.py
+++ wsapp.py
@@ -206,7 +206,7 @@ def initialize_app(flask_app):
 
     # https://www.ebi.ac.uk:443/metabolights/ws/v2
     api.add_resource(reports, res_path + "/v2/reports")
-    api.add_resource(CrossReferencePublicationInformation, res_path + "/v3/europe-pmc-report")
+    api.add_resource(CrossReferencePublicationInformation, res_path + "/v2/europe-pmc-report")
     api.add_resource(curation_log, res_path + "/v2/curation_log")
 
     # ToDo, complete this: api.add_resource(CheckCompounds, res_path + "/ebi-internal/compound-names")
