ee9c64fcb1441f1fd113fba13d90ba2d3aa794f0
cmartin <callumisveryawesome@gmail.com>
2022-01-07 14:37:19 +0000

    still fixing dict referencing issue

--- app/ws/report_builders/europe_pmc_builder.py
+++ app/ws/report_builders/europe_pmc_builder.py
@@ -179,4 +179,4 @@ class EuropePmcReportBuilder:
         fresh_params = self.base_params.cascade({'format': 'DC', 'query': title})
         self.session.headers.update(self.headers_register['citation_ref'])
         response_xml_dict = xmltodict.parse(self.session.get(self.europe_pmc_url, params=fresh_params).text)
-        return response_xml_dict['responseWrapper']['rdf:RDF']['rdf:Description']['dcterms:bibliographicCitation']
+        return response_xml_dict['responseWrapper']['rdf:RDF']['rdf:Description'][0]['dcterms:bibliographicCitation'][0]
