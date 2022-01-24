1fa1415b5eac5428a840a16e98aca7eb35a05da9
calstevemart <callumisveryawesome@gmail.com>
2022-01-24 13:05:16 +0000

    initial commit
    testijng commit squash me

--- app/ws/validation.py
+++ app/ws/validation.py
@@ -1168,6 +1168,7 @@ def validate_assays(isa_study, study_location, validation_schema, override_list,
     validations = []
     all_assay_samples = []
     unique_file_names = []
+    missing_or_incorrect_files = []
 
     study_id = isa_study.identifier
 
@@ -1350,6 +1351,7 @@ def validate_assays(isa_study, study_location, validation_schema, override_list,
                                                          assay_file_list=all_assay_raw_files,
                                                          assay_file_name=a_file_name)
         if not valid:
+            missing_or_incorrect_files.append(file_name)
             err_msg = "File '" + file_name + "'"
             if file_type != unknown_file:
                 err_msg = err_msg + " of type '" + file_type + "'"
@@ -1363,7 +1365,15 @@ def validate_assays(isa_study, study_location, validation_schema, override_list,
             add_msg(validations, val_section, "File '" + file_name + "' found and appears to be correct for column '"
                 + column_name + "'", success, descr=file_description, val_sequence=8.1, log_category=log_category)
 
+    if len(missing_or_incorrect_files) > 0:
+        with open(study_location, 'w') as file:
+            file.write("\n".join(missing_or_incorrect_files))
+    else:
+        missing_or_incorrect_files.append('skippydooppy')
+        with open(study_location, 'w') as file:
+            file.write("\n".join(missing_or_incorrect_files))
     return return_validations(val_section, validations, override_list)
+
 
 
 def get_files_in_sub_folders(study_location):
