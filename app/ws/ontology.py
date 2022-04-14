d882018ac46f0321c30442c72e1e1356f571cfc6
Callum Martin <45428602+calstevemart@users.noreply.github.com>
2022-04-14 11:15:44 +0100

    Update ontology.py

--- app/ws/ontology.py
+++ app/ws/ontology.py
@@ -1404,6 +1404,7 @@ def addEntity(new_term, supclass, definition=None):
 
     all_class = []
     for cls in onto.classes():
+        logger.info(cls)
         all_class += cls.label
 
     if new_term.lower() in [x.lower() for x in all_class]:
@@ -1412,8 +1413,7 @@ def addEntity(new_term, supclass, definition=None):
         abort(400)
         return []
     
-    for class in onto.classes():
-        logger.info(class)
+
     id = getid(onto)
     namespace = onto.get_namespace('http://www.ebi.ac.uk/metabolights/ontology/')
 
