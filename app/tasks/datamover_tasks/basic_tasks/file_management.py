4efd66e1ff6759221ab3b3b4bdb97c5eb6482c89
oyurekten <ozgur@ebi.ac.uk>
2023-06-03 23:51:22 +0100

    remote create folder

--- app/tasks/datamover_tasks/basic_tasks/file_management.py
+++ app/tasks/datamover_tasks/basic_tasks/file_management.py
@@ -1,50 +1,38 @@
-
-import datetime
-import glob
-import json
 import logging
 import os
-import time
 from typing import List, Union
 import celery
 from app.services.storage_service.acl import Acl
-from app.tasks.worker import (MetabolightsTask, celery, get_flask_app, send_email)
-from flask.json import jsonify
-from app.tasks.worker import MetabolightsTask
-from app.utils import MetabolightsException
+from app.tasks.worker import MetabolightsTask, celery
 
-from app.ws.db_connection import update_release_date
-from app.ws.isaApiClient import IsaApiClient
-from app.ws.mtblsWSclient import WsClient
-from app.ws.settings.utils import get_study_settings
-from app.ws.utils import validate_mzml_files, convert_to_isa, copy_file, read_tsv, write_tsv, \
-    update_correct_sample_file_name, get_year_plus_one
-
-logger = logging.getLogger('datamover_worker')
+logger = logging.getLogger("datamover_worker")
 
 
-@celery.task(bind=True, base=MetabolightsTask, name="app.tasks.datamover_tasks.basic_tasks.file_management.create_folders")
-def create_folders(self, folder_paths: Union[str, List[str]], acl: Union[int, Acl] = Acl.AUTHORIZED_READ_WRITE, exist_ok: bool = True):
+@celery.task(
+    bind=True, base=MetabolightsTask, name="app.tasks.datamover_tasks.basic_tasks.file_management.create_folders"
+)
+def create_folders(
+    self, folder_paths: Union[str, List[str]], acl: Union[int, Acl] = Acl.AUTHORIZED_READ_WRITE, exist_ok: bool = True
+):
     results = {}
     if not folder_paths:
         return results
-    paths = []
+    input_paths = []
     if isinstance(folder_paths, str):
-        paths.append(folder_paths)
+        input_paths.append(folder_paths)
     else:
-        paths = folder_paths
-    
+        input_paths = folder_paths
+
     if isinstance(acl, Acl):
         permission = acl.value
     else:
         permission = acl
-           
-    for path_item in paths:
-        exist = True if os.path.exists(path_item) else False
-        is_dir = True if exist and os.path.isdir(path_item) else False
-        
-        if exist:
-            if not is_dir:
+
+    for path_item in input_paths:
+        path_exist = os.path.exists(path_item)
+        print(path_item)
+        if path_exist:
+            if os.path.isdir(path_item):
                 results[path_item] = {"status": False, "message": f"Path '{path_item}' is not a folder"}
             else:
                 current_permission = os.stat(path_item).st_mode & 0o777
@@ -53,22 +41,35 @@ def create_folders(self, folder_paths: Union[str, List[str]], acl: Union[int, Ac
                         os.chmod(path_item, mode=permission)
                         last_permission = os.stat(path_item).st_mode & 0o777
                         if last_permission == permission:
-                            results[path_item] = {"status": True, "message": f"Path '{path_item}'  already exists. Folder permission was updated"}
+                            results[path_item] = {
+                                "status": True,
+                                "message": f"Path '{path_item}'  already exists. Folder permission was updated",
+                            }
                         else:
-                            results[path_item] = {"status": True, "message": f"Path '{path_item}'  already exists. Permission could not be updated."}
-                    except Exception as exc:
-                        results[path_item] = {"status": False, "message": f"Path '{path_item}'  already exists. Permission could not be updated. Exception: {str(exc)}"}
+                            results[path_item] = {
+                                "status": True,
+                                "message": f"Path '{path_item}'  already exists. Permission could not be updated.",
+                            }
+                    except Exception as ex:
+                        results[path_item] = {
+                            "status": False,
+                            "message": f"Path '{path_item}'  already exists. Permission could not be updated. Exception: {str(ex)}",
+                        }
                 else:
                     results[path_item] = {"status": True, "message": f"Path '{path_item}' already exists."}
         else:
             try:
                 os.makedirs(path_item, mode=permission, exist_ok=exist_ok)
-            
-                last_status = True if os.path(path_item) else False
+
+                last_status =  os.path.exists(path_item)
                 if last_status:
                     results[path_item] = {"status": True, "message": f"Path '{path_item}' was created."}
                 else:
                     results[path_item] = {"status": False, "message": f"Path '{path_item}' could not be created."}
-            except Exception as exc:
-                results[path_item] = {"status": False, "message": f"Path '{path_item}' could not be created. Exception: {str(exc)}"}            
+            except Exception as ex:
+                results[path_item] = {
+                    "status": False,
+                    "message": f"Path '{path_item}' could not be created. Exception: {str(ex)}",
+                }
+                
     return results
