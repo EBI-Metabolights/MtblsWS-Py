52fa86e82d497a284a1c197a0d67c0cbeb7b2723
ozgur <ozgur@ebi.ac.uk>
2023-10-10 22:35:04 +0100

    sentinel configuration

--- app/tasks/worker.py
+++ app/tasks/worker.py
@@ -17,10 +17,10 @@ from app.ws.study.user_service import UserService
 settings: CelerySettings = get_settings().celery
 
 rs: RedisConnection = settings.broker
-broker_url = f'redis+sentinel://:{rs.redis_password}@{rs.redis_host}:{rs.redis_port}/{rs.redis_db}'
+#broker_url = f'redis+sentinel://:{rs.redis_password}@{rs.redis_host}:{rs.redis_port}/{rs.redis_db}'
 
 
-# broker_url = f"redis://:{rs.redis_password}@{rs.redis_host}:{rs.redis_port}/{rs.redis_db}"
+broker_url = f"redis://:{rs.redis_password}@{rs.redis_host}:{rs.redis_port}/{rs.redis_db}"
 result_backend = broker_url
 celery = Celery(
     __name__,
@@ -41,8 +41,8 @@ celery = Celery(
     ],
 )
 
-celery.conf.broker_transport_options = { 'master_name': "master-redis-ws", 'sentinel_kwargs': { 'password': rs.redis_password } }
-celery.conf.result_backend_transport_options = {'master_name': "master-redis-ws", 'sentinel_kwargs': { 'password': rs.redis_password }}
+#celery.conf.broker_transport_options = { 'master_name': "master-redis-ws", 'sentinel_kwargs': { 'password': rs.redis_password } }
+#celery.conf.result_backend_transport_options = {'master_name': "master-redis-ws", 'sentinel_kwargs': { 'password': rs.redis_password }}
 
 @lru_cache(1)
 def get_flask_app():
