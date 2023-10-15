293857af6d998c2444f36edaae8414ca45954249
oyurekten <ozgur@ebi.ac.uk>
2023-10-15 14:39:20 +0100

    redis senetinel and k8s hpc cluster support

--- app/tasks/system_monitor_tasks/heartbeat.py
+++ app/tasks/system_monitor_tasks/heartbeat.py
@@ -4,4 +4,4 @@ from billiard import current_process
 @celery.task(bind=True, base=MetabolightsTask, name="app.tasks.system_monitor_tasks.heartbeat.ping")
 def ping(self, input: str):
     process = current_process()
-    return {"input": input, "version": app.__api_version__, "requester": self.request.hostname, "responder": process._target.initargs[1]}
+    return {"reply_for": input, "worker_version": app.__api_version__, "name": self.request.hostname, "celery_name": process._target.initargs[1]}
