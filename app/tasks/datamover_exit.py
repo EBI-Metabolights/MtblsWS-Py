41ed94f290c58fac1d5c0f20d2142abbaace0777
oyurekten <ozgur@ebi.ac.uk>
2023-10-16 08:57:34 +0100

    k8s datamover worker config

--- app/tasks/datamover_exit.py
+++ app/tasks/datamover_exit.py
@@ -10,7 +10,7 @@ from app.tasks.datamover_worker_utils import delete_current_workers, delete_queu
 
 
 if __name__ == "__main__":
-    worker_name = os.getenv('WORKER_NAME')
+    worker_name = os.getenv('WORKER_HOST_NAME')
     
     if not worker_name:
         print("Invalid worker name.")
--- app/tasks/datamover_init.py
+++ app/tasks/datamover_init.py
@@ -8,7 +8,7 @@ from app.tasks.datamover_worker_utils import restart_datamover_worker
 
 
 if __name__ == "__main__":
-    worker_name = os.getenv('WORKER_NAME')
+    worker_name = os.getenv('WORKER_HOST_NAME')
     
     if not worker_name:
         print("Invalid worker name.")
