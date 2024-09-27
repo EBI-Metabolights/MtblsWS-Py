f8199bc359c99616370c62702778fc73cb74854d
oyurekten <103259724+oyurekten@users.noreply.github.com>
2024-09-27 10:36:43 +0100

    mtbls accession number update feature db patch script

--- scripts/MTBLS681.sql
+++ scripts/MTBLS681.sql
@@ -1,2 +1,7 @@
 alter table studies add reserved_accession varchar(50);
 alter table studies add reserved_submission_id varchar(50);
+update studies set reserved_accession = accession, reserved_submission_id = "REQ" || to_char(submissiondate, 'YYYYMMDD') || id ;
+update studies set acc = reserved_submission_id where status = 0 or status > 3;
+
+alter sequence hibernate_sequence cache 1;
+alter sequence hibernate_sequence increment by 1;
