ca090a3aee8d924998ea4f3f0db2aa1433b51230
oyurekten <ozgur@ebi.ac.uk>
2024-11-26 22:01:11 +0000

    .gitlab-ci.yml is updated and deployment folder is removed.

--- deployment/steps/deploy_datamover_proxy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-FILE_PATH=$(dirname "$0")
-PARENT_PATH=$(dirname "$FILE_PATH")
-
-echo "Parent path: $PARENT_PATH"
-bash $PARENT_PATH/deploy.sh metablsws-py-datamover-proxy "$APP_VERSION"
