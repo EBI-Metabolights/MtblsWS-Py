0d7b4ad89eb4715f908f845780b4ce110ada604c
oyurekten <ozgur@ebi.ac.uk>
2025-01-14 14:46:38 +0000

    datadabase update script

--- scripts/mtbls_accession_db_update.sql
+++ scripts/mtbls_accession_db_update.sql
@@ -5,7 +5,7 @@ alter table studies add first_public_date timestamp;
 update studies set reserved_submission_id = 'REQ' || to_char(submissiondate, 'YYYYMMDD') || id where reserved_submission_id is null;
 update studies set reserved_accession = acc where acc like 'MTBLS%';
 update studies set first_public_date = releasedate where status = 3;
-
+alter table studies alter column curation_request set default 1;
 -- update studies set acc = reserved_submission_id where status = 0 or status > 3;
 
 alter sequence hibernate_sequence cache 1;
