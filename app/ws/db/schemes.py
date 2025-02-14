679d8c36226b66ce7c86fd0b7ae38d6f07fe4d25
oyurekten <ozgur@ebi.ac.uk>
2025-02-14 14:15:05 +0000

    First private date column is added to track study accessioned date. submittiondate will be used to track study creation date. There is an sql in scripts folder to update database

--- app/ws/db/schemes.py
+++ app/ws/db/schemes.py
@@ -100,6 +100,7 @@ class Study(Base):
     reserved_accession = Column(Text, nullable=True)
     reserved_submission_id = Column(Text, nullable=True)
     first_public_date = Column(DateTime, nullable=True)
+    first_private_date = Column(DateTime, nullable=True)
     users = relationship('User', secondary='study_user', back_populates="studies")
 
 
