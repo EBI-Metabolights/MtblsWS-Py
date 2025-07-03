2eb5f484dbf4ad628028d0f57b443535ae24d35c
oyurekten <ozgury@ebi.ac.uk>
2025-07-03 13:30:38 +0000

    send email task error message is updated

---
+++ scripts/email_tasks/emails_by_status/submitted/send_email.py
@@ -0,0 +1,86 @@
+import datetime
+import logging
+import pathlib
+from jinja2 import Environment, PackageLoader, select_autoescape
+
+from scripts.email_tasks.models import MetaboLightsStudyReport, load_study_report
+from scripts.email_tasks.utils import send_task_email
+
+
+logger = logging.getLogger(__name__)
+
+
+if __name__ == "__main__":
+    task_name = "STUDY_SEND_METABOLIGHTS_UPDATES_EMAIL"
+    env = Environment(
+        loader=PackageLoader("scripts.email_tasks.emails_by_status", "submitted"),
+        autoescape=select_autoescape(["html", "xml"]),
+    )
+
+    report: MetaboLightsStudyReport = load_study_report(
+        study_report_path="study_report.json"
+    )
+    exclue_submitter_emails = set()
+    exclue_submitter_emails.update(
+        [
+            "placeholder@ebi.ac.uk",
+            "metabolights-help@ebi.ac.uk",
+        ]
+    )
+    max_created_at = datetime.datetime.fromisoformat("2025-06-13 09:00:00.00000")
+    studies = report.filter_study_report(
+        status="provisional",
+        exclude_submitter_emails=exclue_submitter_emails,
+        min_created_at=None,
+        max_created_at=max_created_at,
+    )
+
+    script_template_name = "submitted_studies.html"
+    template = env.get_template(script_template_name)
+
+    pathlib.Path("submitted_studies_email.txt")
+
+    for idx, study in enumerate(studies, start=1): 
+        try:
+            subject_name = f"MetaboLights Study Status Update - {study.study_id}"
+            if len(study.submitters) == 1:
+                submitter_fullname = ", ".join([x.full_name for x in study.submitters])
+            else:
+                submitter_fullname = ", ".join(
+                    [
+                        x.full_name
+                        for idx, x in enumerate(study.submitters)
+                        if idx < len(study.submitters)
+                    ]
+                )
+                submitter_fullname += " and " + study.submitters[-1].full_name
+            inputs = {
+                "submitter_fullname": submitter_fullname,
+                "metabolights_website_url": "https://www.ebi.ac.uk/metabolights",
+                "study_id": study.study_id
+                ## more keys in template
+            }
+            body = template.render(inputs)
+            from_mail_address = "metabolights-no-reply@ebi.ac.uk"
+            to_mail_addresses = []
+            for x in study.submitters:
+                to_mail_addresses.extend(x.emails)
+
+            cc_mail_addresses = []
+            for x in study.contacts:
+                cc_mail_addresses.extend(x.emails)
+
+            send_task_email(
+                study_id=study.study_id,
+                task_name=task_name,
+                subject_name=subject_name,
+                body=body,
+                from_mail_address=from_mail_address,
+                to_mail_addresses="ozgury@ebi.ac.uk",
+                cc_mail_addresses="ozgur.yurekten@gmail.com",
+                reply_to="metabolights-help@ebi.ac.uk",
+            )
+            if idx > 1:
+                break
+        except Exception as ex:
+            raise ex
---
+++ scripts/email_tasks/utils.py
@@ -0,0 +1,126 @@
+import datetime
+import logging
+
+from app.tasks.worker import get_email_service, get_flask_app
+from app.ws.db.dbmanager import DBManager
+from app.ws.db.schemes import StudyTask
+from app.ws.db.types import StudyTaskStatus
+from app.ws.study.study_service import StudyService
+from email_validator import validate_email
+
+logger = logging.getLogger(__name__)
+
+flask_app = get_flask_app()
+email_service = get_email_service(flask_app=flask_app)
+
+
+def send_task_email(
+    study_id: str,
+    task_name: str,
+    subject_name: str,
+    body: str,
+    from_mail_address,
+    to_mail_addresses,
+    cc_mail_addresses=None,
+    reply_to=None,
+) -> bool:
+    task = None
+    try:
+        task = create_task(study_id=study_id, task_name=task_name)
+    except Exception as ex:
+        logger.error(str(ex))
+    if not task:
+        return False
+    with flask_app.app_context():
+        try:
+            if not from_mail_address:
+                raise Exception("From email address is empty")
+            if not to_mail_addresses:
+                raise Exception("To email address is empty")
+
+            if not isinstance(cc_mail_addresses, list):
+                cc_mail_addresses = cc_mail_addresses.split(",")
+            if not isinstance(to_mail_addresses, list):
+                to_mail_addresses = to_mail_addresses.split(",")
+            for emails in (to_mail_addresses, cc_mail_addresses):
+                for email in emails:
+                    validate_email(email)
+
+            email_service.send_generic_email(
+                subject_name=subject_name,
+                body=body,
+                from_mail_address=from_mail_address,
+                to_mail_addresses=to_mail_addresses,
+                cc_mail_addresses=cc_mail_addresses if cc_mail_addresses else None,
+                reply_to=reply_to,
+                fail_silently=False,
+            )
+            complete_task(
+                study_id,
+                task_name=task_name,
+                status=StudyTaskStatus.EXECUTION_SUCCESSFUL,
+                message="Email is sent.",
+            )
+            return True
+        except Exception as ex:
+            complete_task(
+                study_id,
+                task_name=task_name,
+                status=StudyTaskStatus.EXECUTION_FAILED,
+                message=f"Email is not sent. {str(ex)}",
+            )
+            return False
+
+
+def complete_task(study_id, task_name: str, status: str, message: str) -> StudyTask:
+    tasks = StudyService.get_instance().get_study_tasks(
+        study_id=study_id, task_name=task_name
+    )
+
+    with DBManager.get_instance().session_maker() as db_session:
+        if tasks:
+            task: StudyTask = tasks[0]
+            task.last_execution_message = message
+            task.last_execution_status = status
+            task.last_request_executed = task.last_request_time
+            db_session.add(task)
+            db_session.commit()
+        else:
+            logger.error("%s %s There is no task", study_id, task_name)
+            # raise Exception(f"{study_id} {task_name}. There is no task")
+
+
+def create_task(study_id, task_name: str) -> StudyTask:
+    tasks = StudyService.get_instance().get_study_tasks(
+        study_id=study_id, task_name=task_name
+    )
+    with DBManager.get_instance().session_maker() as db_session:
+        if tasks:
+            task: StudyTask = tasks[0]
+        else:
+            now = datetime.datetime.now(datetime.UTC)
+            task = StudyTask()
+            task.study_acc = study_id
+            task.task_name = task_name
+            task.last_request_time = now
+            task.last_execution_time = now
+            task.last_request_executed = now
+            task.last_execution_status = StudyTaskStatus.NOT_EXECUTED
+            task.last_execution_message = "Task is initiated."
+
+        if task.last_execution_status in {
+            StudyTaskStatus.NOT_EXECUTED,
+            StudyTaskStatus.EXECUTION_FAILED,
+        }:
+            task.last_execution_status = StudyTaskStatus.EXECUTING
+            task.last_execution_time = task.last_request_time
+            task.last_request_executed = task.last_execution_time
+            task.last_execution_message = "Task is initiated."
+            db_session.add(task)
+            db_session.commit()
+            return task
+        else:
+            raise Exception(
+                f"{study_id} task {task_name} failed. "
+                f"Task status is {task.last_execution_status}."
+            )
