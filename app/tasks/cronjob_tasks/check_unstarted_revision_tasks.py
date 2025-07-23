cf417b2cca2e453754974ecdeb64e12902b16127
oyurekten <ozgury@ebi.ac.uk>
2025-07-23 15:20:29 +0000

    enable logger

--- app/tasks/cronjob_tasks/check_unstarted_revision_tasks.py
+++ app/tasks/cronjob_tasks/check_unstarted_revision_tasks.py
@@ -1,5 +1,17 @@
-from app.tasks.common_tasks.curation_tasks.study_revision import check_not_started_study_revisions
+import logging
+import sys
+from app.tasks.common_tasks.curation_tasks.study_revision import (
+    check_not_started_study_revisions,
+)
 
+logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO,
+        format="[%(asctime)s] %(levelname)s [%(moduleName)s] %(message)s",
+        datefmt="%d/%b/%Y %H:%M:%S",
+        stream=sys.stdout,
+    )
+    logger.setLevel(logging.DEBUG)
     check_not_started_study_revisions()
