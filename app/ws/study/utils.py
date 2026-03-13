07ec677f6405df5952d06a31de8bb4793ba46089
oyurekten <ozgury@ebi.ac.uk>
2026-03-13 07:06:07 +0000

    sync with test branch

--- app/ws/study/utils.py
+++ app/ws/study/utils.py
@@ -8,3 +8,17 @@ def get_study_metadata_path(study_id: str):
     return os.path.join(
         study_settings.mounted_paths.study_metadata_files_root_path, study_id
     )
+
+
+def get_study_audit_files_path(study_id: str):
+    study_settings = get_settings().study
+    return os.path.join(
+        study_settings.mounted_paths.study_audit_files_root_path, study_id, "audit"
+    )
+
+
+def get_study_internal_files_path(study_id: str):
+    study_settings = get_settings().study
+    return os.path.join(
+        study_settings.mounted_paths.study_internal_files_root_path, study_id
+    )
