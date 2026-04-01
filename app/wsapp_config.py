3c95844f0f47fb47e9f906b8584dbe41fab25e49
Felix Amaladoss <famaladoss@ebi.ac.uk>
2026-04-01 14:44:56 +0000

    Fix public study list itr and add health endpoint

--- app/wsapp_config.py
+++ app/wsapp_config.py
@@ -17,7 +17,7 @@
 #  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 
 import socket
-
+from flask import jsonify, Blueprint
 from flask_cors import CORS
 from flask_mail import Mail
 from flask_restful import Api
@@ -321,6 +321,19 @@ def initialize_app(flask_app):
         api_spec_url=api_doc,
         resourcePath=context_path,
     )
+    
+    ws_bp = Blueprint('metabolights_ws', __name__, url_prefix=context_path)
+    @ws_bp.route('/health', methods=['GET'])
+    def health_check():
+        # Return 200 OK for K8s Liveness/Readiness probes
+        return jsonify({
+            "status": "up",
+            "app": "metabolights-ws",
+            "version": get_settings().server.description.metabolights_api_version
+        }), 200
+
+    flask_app.register_blueprint(ws_bp)
+    
     res_path = context_path
     api.add_resource(About, res_path)
     api.add_resource(AboutServer, res_path + "/ebi-internal/server-info")
